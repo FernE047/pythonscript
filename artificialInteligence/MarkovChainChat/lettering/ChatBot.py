cb7a833f33775cb142c2ccaa71dd66269cb3b697
Camilla Alves <arcamilla42@gmail.com>
2026-01-28 14:38:30 -0300

    refactor: rename and improve fetch_character_from_chain and generate_message functions for clarity

--- artificialInteligence/MarkovChainChat/lettering/ChatBot.py
+++ artificialInteligence/MarkovChainChat/lettering/ChatBot.py
@@ -1,42 +1,41 @@
 from random import randint
 
 
-def getAnChar(indice, anterior=""):
-    nome = "chain//{0:03d}.txt"
-    file = open(nome.format(indice), encoding="utf-8")
-    linha = file.readline()
-    data = {}
-    while linha:
-        if indice == 0:
-            letra = linha[0]
-            numero = int(linha[2:-1])
-        else:
-            if anterior == linha[0]:
-                letra = linha[2]
-                numero = int(linha[4:-1])
+def fetch_character_from_chain(index: int, previous_character: str = "") -> str:
+    with open(f"chain/{index:03d}.txt", "r", encoding="utf-8") as file:
+        line = file.readline()
+        character_frequency_map: dict[str, int] = {}
+        while line:
+            if index != 0:
+                if previous_character != line[0]:
+                    line = file.readline()
+                    continue
+                character = line[2]
+                frequency_count = int(line[4:-1])
             else:
-                linha = file.readline()
-                continue
-        data[letra] = numero
-        linha = file.readline()
-    total = sum(list(data.values()))
-    escolhido = randint(1, total)
-    soma = 0
-    for indice, valor in enumerate(data.values()):
-        soma += valor
-        if soma >= escolhido:
-            file.close()
-            return list(data.keys())[indice]
+                character = line[0]
+                frequency_count = int(line[2:-1])
+            character_frequency_map[character] = frequency_count
+            line = file.readline()
+        frequencies = list(character_frequency_map.values())
+        total = sum(frequencies)
+        chosen = randint(1, total)
+        cumulative = 0
+        for index, value in enumerate(frequencies):
+            cumulative += value
+            if cumulative >= chosen:
+                return list(character_frequency_map.keys())[index]
+    return ""
 
 
-def doAMessage():
-    mensagem = ""
-    letra = getAnChar(0)
-    while letra != "¨":
-        mensagem += letra
-        letra = getAnChar(len(mensagem), letra)
-    return mensagem
+def generate_message() -> str:
+    message = ""
+    letter = fetch_character_from_chain(0)
+    while letter != "¨":
+        message += letter
+        letter = fetch_character_from_chain(len(message), letter)
+    return message
 
 
-for a in range(1000):
-    print(str(a) + " : " + doAMessage())
+for index in range(1000):
+    print(str(index) + " : " + generate_message())
