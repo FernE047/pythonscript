9a89e0a4f151e0140d2bc1fa3fa3d29caf3cc172
Camilla Alves <arcamilla42@gmail.com>
2026-01-28 08:50:24 -0300

    refactor: restructure job listing functions and improve data handling in procuraEstagio.py

--- ajudaTarefas/ProcuraEstagio/procuraEstagio.py
+++ ajudaTarefas/ProcuraEstagio/procuraEstagio.py
@@ -1,62 +1,141 @@
-import requests, bs4, re, webbrowser
-import internet
-import textos
-import userUtil
+from typing import Literal, TypedDict, cast, overload
+import webbrowser
 
-def apenasVagas(informacao):
-    vagas = []
-    for info in informacao:
-        descricaoTalvez = info.select('.descricao-vaga')
-        if(descricaoTalvez):
-            titulo = info.get('title')
-            site = info.get('href')
-            descricao = info.find('p').getText()
-            vagas.append({'titulo' : titulo, 'site' : site, 'descricao' : descricao})
-    return(vagas)
+from bs4 import ResultSet
+from bs4.element import Tag
 
-def limpaVagas(vagas,pesquisa,procura):
-    vagasLimpas = []
-    for vaga in vagas:
-        for termo in pesquisa:
-            termo = termo.lower()
-            for chave in procura:
-                texto = vaga[chave].lower()
-                palavras = textos.separaPalavras(texto)
-                if(termo in palavras):
-                    vagasLimpas.append(vaga)
-                    break
-    return(vagasLimpas)
 
-sites = ["http://empregacampinas.com.br/page/{}/?s=est%C3%A1gio","http://empregacampinas.com.br/categoria/vaga/page/{}/"]
-escolha = userUtil.entradaNaLista("",["estágio","emprego","tudo"],retorno="numerico")
-if(escolha<2):
-    sites = [sites[escolha]]
-local = ["Campinas","Sumaré","Sumare"]
-termos = ["e-commerce","TI","tecnologia","programador","programadores","Web"]
-navegador=webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s")
-vagasFinais=[]
+class jobListingData(TypedDict):
+    title: str
+    site: str
+    description: str
+
+
+@overload
+def choose_from_options(
+    prompt: str, options: list[str], mode: Literal["text"]
+) -> str: ...
+
+
+@overload
+def choose_from_options(
+    prompt: str, options: list[str], mode: Literal["number"]
+) -> int: ...
+
+
+def choose_from_options(
+    prompt: str, options: list[str], mode: Literal["text", "number"] = "text"
+) -> str | int:
+    while True:
+        for i, option in enumerate(options):
+            print(f"{i} - {option}")
+        user_choice = input(prompt)
+        try:
+            if mode == "number":
+                return int(user_choice)
+            else:
+                return options[int(user_choice)]
+        except (ValueError, IndexError):
+            user_choice = input("not valid, try again: ")
+
+
+def get_tag_site(url: str, css_selector: str) -> ResultSet[Tag]:
+    import requests
+    import bs4
+
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    information = soup.select(css_selector)
+    return information
+
+
+def filter_job_listing(information: ResultSet[Tag]) -> list[jobListingData]:
+    job_listings: list[jobListingData] = []
+    for info in information:
+        if not info.select(".descricao-vaga"):
+            continue
+        title = cast(str, info.get("title", ""))
+        site = cast(str, info.get("href", ""))
+        description = ""
+        description_tag = info.find("p")
+        if description_tag:
+            description = description_tag.get_text(strip=True)
+        job_listings.append({"title": title, "site": site, "description": description})
+    return job_listings
+
+
+def is_keywords_in_job(
+    job: jobListingData,
+    search_terms: list[str],
+    keys: list[Literal["title", "description"]],
+) -> bool:
+    for term in search_terms:
+        term = term.lower()
+        for key in keys:
+            text = job[key].lower()
+            words = text.split(" ")
+            if term in words:
+                return True
+    return False
+
+
+def refine_job_results(
+    jobs: list[jobListingData],
+    search_terms: list[str],
+    keys: list[Literal["title", "description"]],
+) -> list[jobListingData]:
+    refined_jobs: list[jobListingData] = []
+    for job in jobs:
+        if is_keywords_in_job(job, search_terms, keys):
+            refined_jobs.append(job)
+    return refined_jobs
+
+
+sites = [
+    "http://empregacampinas.com.br/page/{}/?s=est%C3%A1gio",
+    "http://empregacampinas.com.br/categoria/vaga/page/{}/",
+]
+choice = choose_from_options("", ["estágio", "emprego", "tudo"], mode="number")
+if choice < 2:
+    sites = [sites[choice]]
+search_locations = ["Campinas", "Sumaré", "Sumare"]
+search_keywords = [
+    "e-commerce",
+    "TI",
+    "tecnologia",
+    "programador",
+    "programadores",
+    "Web",
+]
+browser = webbrowser.get(
+    "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+)
+final_job_listings: list[jobListingData] = []
 for site in sites:
-    for a in range(1,10):
-        informacao = internet.siteProcura(site.format(a),'.thumbnail')
-        vagasDisponiveis = apenasVagas(informacao)
-        if(local):
-            vagasDisponiveis = limpaVagas(vagasDisponiveis,local,['titulo'])
-        if(termos):
-            vagasDisponiveis = limpaVagas(vagasDisponiveis,termos,['titulo','descricao'])
-        vagasFinais+=vagasDisponiveis
+    for page in range(1, 10):
+        information = get_tag_site(site.format(page), ".thumbnail")
+        vagasDisponiveis = filter_job_listing(information)
+        if search_locations:
+            vagasDisponiveis = refine_job_results(
+                vagasDisponiveis, search_locations, ["title"]
+            )
+        if search_keywords:
+            vagasDisponiveis = refine_job_results(
+                vagasDisponiveis, search_keywords, ["title", "description"]
+            )
+        final_job_listings.extend(vagasDisponiveis)
 while True:
-    opcoes=[vaga['titulo'] for vaga in vagasFinais]
-    escolha=userUtil.entradaNaLista("escolha : ",opcoes+["todos","sair"],retorno='numerico')
-    if(escolha<len(opcoes)):
-        navegador.open(vagasFinais[escolha]['site'])
-        vagasFinais.pop(escolha)
-    else:
-        escolha-=len(opcoes)
-        if(escolha==0):
-            for vaga in vagasFinais:
-                navegador.open(vaga['site'])
-                vagasFinais=[]
+    options = [job["title"] for job in final_job_listings]
+    choice = choose_from_options(
+        "choose : ", options + ["all", "exit"], mode="number"
+    )
+    if choice < len(options):
+        browser.open(final_job_listings[choice]["site"])
+        final_job_listings.pop(choice)
+        continue
+    choice -= len(options)
+    if choice != 0:
         break
-                
-            
-    
+    for job in final_job_listings:
+        browser.open(job["site"])
