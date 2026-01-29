2ae5c0677cb30be0d11a7171fb15913d44968b35
Camilla Alves <arcamilla42@gmail.com>
2026-01-29 14:08:54 -0300

    refactor: rename and improve character and word generation functions for clarity and consistency

--- artificialInteligence/MarkovChainChat/wordMaker/MarkovMultiSimpleNoSpace/markovMaker.py
+++ artificialInteligence/MarkovChainChat/wordMaker/MarkovMultiSimpleNoSpace/markovMaker.py
@@ -1,41 +1,40 @@
 from random import randint
 
 
-def getAnChar(nome, indice, anterior=""):
-    nome += "//{0:03d}.txt"
-    file = open(nome.format(indice), encoding="utf-8")
-    linha = file.readline()
-    data = {}
-    while linha:
-        if indice == 0:
-            letra = linha[0]
-            numero = int(linha[2:-1])
-        else:
-            if anterior == linha[0]:
-                letra = linha[2]
-                numero = int(linha[4:-1])
+def generate_char(file_name: str, index: int, previous_char: str = "") -> str:
+    with open(f"{file_name}/{index:03d}.txt", encoding="utf-8") as file:
+        line = file.readline()
+        character_weights: dict[str, int] = {}
+        while line:
+            if index == 0:
+                char = line[0]
+                weight = int(line[2:-1])
             else:
-                linha = file.readline()
-                continue
-        data[letra] = numero
-        linha = file.readline()
-    total = sum(list(data.values()))
-    escolhido = randint(1, total)
-    soma = 0
-    for indice, valor in enumerate(data.values()):
-        soma += valor
-        if soma >= escolhido:
-            file.close()
-            return list(data.keys())[indice]
+                if previous_char == line[0]:
+                    char = line[2]
+                    weight = int(line[4:-1])
+                else:
+                    line = file.readline()
+                    continue
+            character_weights[char] = weight
+            line = file.readline()
+        total = sum(list(character_weights.values()))
+        chosen = randint(1, total)
+        cumulative_frequency = 0
+        for index, value in enumerate(character_weights.values()):
+            cumulative_frequency += value
+            if cumulative_frequency >= chosen:
+                return list(character_weights.keys())[index]
+        return ""
 
 
-def doAWord(nome):
-    mensagem = ""
-    letra = getAnChar(nome, 0)
-    while letra != "¨":
-        mensagem += letra
-        letra = getAnChar(nome, len(mensagem), letra)
-    return mensagem
+def generate_word(file_name: str) -> str:
+    generated_word = ""
+    char = generate_char(file_name, 0)
+    while char != "¨":
+        generated_word += char
+        char = generate_char(file_name, len(generated_word), char)
+    return generated_word
 
 
 def get_file_name() -> str:
@@ -54,5 +53,5 @@ def get_file_name() -> str:
 
 
 file_name = get_file_name()
-for a in range(1000):
-    print(str(a) + " : " + doAWord(file_name))
+for index in range(1000):
+    print(f"{index} : {generate_word(file_name)}")
