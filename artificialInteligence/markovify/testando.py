75045e90e6e8b3a06c014c39fe3ca9952f290dce
Camilla Alves <arcamilla42@gmail.com>
2026-01-29 14:51:21 -0300

    fix: add type ignore comments for markovify combine function to suppress type checking errors

--- artificialInteligence/markovify/testando.py
+++ artificialInteligence/markovify/testando.py
@@ -1,11 +1,18 @@
+import markovify  # type: ignore
+import os
+
+"""
+markovify doesn't ahve a stub for combine, so we need to ignore the type checking for that line.
+"""
+
 combined_model = None
-for (dirpath, _, filenames) in os.walk("path/to/my/huge/corpus"):
+for dirpath, _, filenames in os.walk("path/to/my/huge/corpus"):
     for filename in filenames:
         with open(os.path.join(dirpath, filename)) as f:
             model = markovify.Text(f, retain_original=False)
             if combined_model:
-                combined_model = markovify.combine(models=[combined_model, model])
+                combined_model = markovify.combine(models=[combined_model, model])  # type: ignore
             else:
                 combined_model = model
 
-print(combined_model.make_sentence())
+print(combined_model.make_sentence())  # type: ignore
