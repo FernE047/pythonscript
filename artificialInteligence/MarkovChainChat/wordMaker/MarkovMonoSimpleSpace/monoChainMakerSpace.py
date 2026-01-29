984b698dce8100b564411ce31386621b2fb85edd
Camilla Alves <arcamilla42@gmail.com>
2026-01-29 09:44:08 -0300

    refactor: improve chain file update logic and enhance term frequency handling

--- artificialInteligence/MarkovChainChat/wordMaker/MarkovMonoSimpleSpace/monoChainMakerSpace.py
+++ artificialInteligence/MarkovChainChat/wordMaker/MarkovMonoSimpleSpace/monoChainMakerSpace.py
@@ -1,4 +1,7 @@
 import os
+from typing import cast
+
+ChainData = tuple[str, str]
 
 
 def rename_file(source_file_name: str, destination_file_name: str) -> None:
@@ -10,31 +13,32 @@ def rename_file(source_file_name: str, destination_file_name: str) -> None:
         destination_file.write(content)
 
 
-def alteraMonoChainFile(nome, termo, indice):
-    nomeTemp = f"{nome}//c.txt"
-    nomeReal = f"{nome}//{indice:03d}.txt"
-    fileWrite = open(nomeTemp, "w", encoding="UTF-8")
-    if f"{indice:03d}.txt" in os.listdir(nome):
-        fileRead = open(nomeReal, "r", encoding="UTF-8")
-        linha = fileRead.readline()
-        encontrou = False
-        indice = len(termo)
-        while linha:
-            palavras = linha.split()
-            if palavras[:-1] == termo:
-                palavras[-1] = str(int(palavras[-1]) + 1)
-                fileWrite.write(" ".join(palavras) + "\n")
-                encontrou = True
-            else:
-                fileWrite.write(linha)
-            linha = fileRead.readline()
-        if not encontrou:
-            fileWrite.write(" ".join(termo) + " 1\n")
-        fileRead.close()
-    else:
-        fileWrite.write(" ".join(termo) + " 1\n")
-    fileWrite.close()
-    renome(nomeTemp, nomeReal)
+def update_chain(file_name: str, chain_terms: ChainData, index: int) -> None:
+    update_chain_file(file_name, chain_terms, index)
+    rename_file(f"{file_name}/c.txt", f"{file_name}/{index:03d}.txt")
+
+
+def update_chain_file(file_name: str, chain_terms: ChainData, index: int) -> None:
+    with open(f"{file_name}/c.txt", "w", encoding="UTF-8") as file_write:
+        if f"{index:03d}.txt" not in os.listdir(file_name):
+            file_write.write(" ".join(chain_terms) + " 1\n")
+            return
+        with open(f"{file_name}/{index:03d}.txt", "r", encoding="UTF-8") as file_read:
+            line = file_read.readline()
+            term_exists = False
+            while line:
+                characters = line.split()
+                terms = cast(ChainData, tuple(characters[:-1]))
+                if terms == chain_terms:
+                    frequency = int(characters[-1]) + 1
+                    terms_flat = " ".join(terms)
+                    file_write.write(f"{terms_flat} {frequency}\n")
+                    term_exists = True
+                else:
+                    file_write.write(line)
+                line = file_read.readline()
+            if not term_exists:
+                file_write.write(" ".join(chain_terms) + " 1\n")
 
 
 is_file_name_valid = False
@@ -52,35 +56,34 @@ while not is_file_name_valid:
                 os.mkdir(file_name)
     except Exception as _:
         print("invalid name")
-linha = file.readline()[:-1]
-palavraQuant = []
-while linha:
-    palavras = linha.split()
-    while len(palavras) > len(palavraQuant):
-        palavraQuant.append(0)
-    palavraQuant[len(palavras) - 1] += 1
-    for m, palavra in enumerate(palavras):
-        tamanho = len(palavra)
-        for n in range(tamanho):
-            letra = palavra[n]
-            if n == 0:
-                alteraMonoChainFile(nome, ["¨", letra], m)
-                if tamanho == 1:
-                    alteraMonoChainFile(nome, [letra, "¨"], m)
-                    break
-            if tamanho > 1:
-                if n >= tamanho - 1:
-                    letraSeguinte = "¨"
-                else:
-                    letraSeguinte = palavra[n + 1]
-                alteraMonoChainFile(nome, [letra, letraSeguinte], m)
-                if letraSeguinte == "¨":
-                    break
-    linha = file.readline()[:-1]
-arqInput = open(nome + "//c.txt", "w", encoding="UTF-8")
-for index, quantity in enumerate(palavraQuant):
-    arqInput.write(f"{index} ")
-    arqInput.write(f"{quantity}\n")
-arqInput.close()
-print(tamanho)
-file.close()
+with open(f"{file_name}.txt", "r", encoding="UTF-8") as file:
+    line = file.readline()[:-1]
+    character_frequency_map:list[int] = []
+    while line:
+        words = line.split()
+        while len(words) > len(character_frequency_map):
+            character_frequency_map.append(0)
+        character_frequency_map[len(words) - 1] += 1
+        for word_index, word in enumerate(words):
+            word_length = len(word)
+            for char_index in range(word_length):
+                char = word[char_index]
+                if char_index == 0:
+                    update_chain(file_name, ("¨", char), word_index)
+                    if word_length == 1:
+                        update_chain(file_name, (char, "¨"), word_index)
+                        break
+                if word_length > 1:
+                    if char_index >= word_length - 1:
+                        next_char = "¨"
+                    else:
+                        next_char = word[char_index + 1]
+                    update_chain(file_name, (char, next_char), word_index)
+                    if next_char == "¨":
+                        break
+            print(word_length)
+        line = file.readline()[:-1]
+    with open(f"{file_name}/c.txt", "w", encoding="UTF-8") as character_count_file:
+        for index, quantity in enumerate(character_frequency_map):
+            character_count_file.write(f"{index} ")
+            character_count_file.write(f"{quantity}\n")
