85d5f6dbb849f6f34c0dc9a3bcb07e404df9ae14
Camilla Alves <arcamilla42@gmail.com>
2026-02-02 16:52:11 -0300

    refactor: restructure BrainF interpreter with improved bracket counting and execution logic

--- computacao/BrainF___/BFInterpreterInfinite.py
+++ computacao/BrainF___/BFInterpreterInfinite.py
@@ -1,165 +1,201 @@
-def testaSeAcaba(programa):
-    loop=0
-    for elemento in programa:
-        if(elemento=="["):
-            loop+=1
-        elif(elemento=="]"):
-            loop-=1
-    return(loop)
+from typing import cast
 
-def saiDoLoop(programa,leitura):
-    quantidadeAbertura=0
+
+class Config:
+    def __init__(self):
+        self.debug = False
+        self.debug_final = False
+        self.limit_exec = 10**4
+        self.limit_value = self.limit_exec
+        self.limit_steps = 0
+        self.isOut = 2
+        self.steps = 0
+
+
+def count_brackets(program_script: str) -> int:
+    loop = 0
+    for char in program_script:
+        if char == "[":
+            loop += 1
+        elif char == "]":
+            loop -= 1
+    return loop
+
+
+def findLoopEnd(program_script:str, read_index: int) -> int:
+    open_bracket_count = 0
     while True:
-        leitura+=1
-        elemento=programa[leitura]
-        if(elemento=="["):
-            quantidadeAbertura+=1
-        if(elemento=="]"):
-            if(quantidadeAbertura==0):
+        read_index += 1
+        current_character = program_script[read_index]
+        if current_character == "[":
+            open_bracket_count += 1
+        if current_character == "]":
+            if open_bracket_count == 0:
                 break
             else:
-                quantidadeAbertura-=1
-    return(leitura)
+                open_bracket_count -= 1
+    return read_index
 
-def execucao(programa,tira,leitura=0,index=0):
-    global DEBUG
-    global DEBUGFINAL
-    global isOut
-    global passos
-    global LIMITEEXEC
-    global LIMITEPASSOS
-    global LIMITEVALOR
-    ERROINES=False
+
+def execute_program(
+    program_script: str,
+    memory_tape: list[int],
+    config: Config,
+    read_index: int = 0,
+    cursor: int = 0,
+) -> list[int] | tuple[int, int, list[int]]:
+    is_giving_error = False
     while True:
-        elemento=programa[leitura]
-        if(DEBUGFINAL):
-            passos+=1
-        if(DEBUG):
-            print(elemento,end=" ")
-        if(elemento=="+"):
-            tira[index]+=1
-        elif(elemento=="-"):
-            tira[index]-=1
-        elif(elemento==">"):
-            index+=1
-            if(index>=len(tira)):
-                tira.append(0)
-        elif(elemento=="<"):
-            if(index==0):
-                tira=[0]+tira
+        current_instruction = program_script[read_index]
+        if config.debug_final:
+            config.steps += 1
+        if config.debug:
+            print(current_instruction, end=" ")
+        if current_instruction == "+":
+            memory_tape[cursor] += 1
+        elif current_instruction == "-":
+            memory_tape[cursor] -= 1
+        elif current_instruction == ">":
+            cursor += 1
+            if cursor >= len(memory_tape):
+                memory_tape.append(0)
+        elif current_instruction == "<":
+            if cursor == 0:
+                memory_tape = [0] + memory_tape
             else:
-                index-=1
-        elif(elemento==","):
-            isOut=0
-            tira[index]=int(input("\nentrada:"))
-        elif(elemento=="."):
-            if(isOut==2):
-                isOut=1
-                print("saida:"+str(tira[index]),end="")
-            elif(isOut==1):
-                print(" "+str(tira[index]),end="")
+                cursor -= 1
+        elif current_instruction == ",":
+            config.isOut = 0
+            memory_tape[cursor] = int(input("\nentrada:"))
+        elif current_instruction == ".":
+            if config.isOut == 2:
+                config.isOut = 1
+                print("saida:" + str(memory_tape[cursor]), end="")
+            elif config.isOut == 1:
+                print(" " + str(memory_tape[cursor]), end="")
             else:
-                isOut=1
-                print("\nsaida:"+str(tira[index]),end="")
-        elif(elemento=="["):
-            if(DEBUG):
+                config.isOut = 1
+                print("\nsaida:" + str(memory_tape[cursor]), end="")
+        elif current_instruction == "[":
+            if config.debug:
                 print("")
-            if(tira[index]):
-                leituraInicial=leitura+1
+            if memory_tape[cursor]:
+                initial_read_index = read_index + 1
                 try:
-                    while(tira[index]):
-                        index,leitura,tira=execucao(programa,tira,leituraInicial,index)
-                except:
+                    while memory_tape[cursor]:
+                        cursor, read_index, memory_tape = cast(
+                            tuple[int, int, list[int]],
+                            execute_program(
+                                program_script,
+                                memory_tape,
+                                config,
+                                initial_read_index,
+                                cursor,
+                            ),
+                        )
+                except IndexError:
                     print("Erro")
-                    ERROINES=True
+                    is_giving_error = True
             else:
-                leitura=saiDoLoop(programa,leitura)
-        elif(elemento=="]"):
-            return((index,leitura,tira))
-        if(len(tira)>=LIMITEEXEC):
-            if(not(ERROINES)):
+                read_index = findLoopEnd(program_script, read_index)
+        elif current_instruction == "]":
+            return (cursor, read_index, memory_tape)
+        if len(memory_tape) >= config.limit_exec:
+            if not (is_giving_error):
                 print("limite de execução")
             break
-        if((tira[index]>=LIMITEVALOR)or(tira[index]<=-LIMITEVALOR)):
-            if(not(ERROINES)):
+        if (memory_tape[cursor] >= config.limit_value) or (
+            memory_tape[cursor] <= -config.limit_value
+        ):
+            if not (is_giving_error):
                 print("limite de valor excedido pelo indice:")
-                print(index)
+                print(cursor)
                 print("valor:")
-                print(tira[index])
+                print(memory_tape[cursor])
             break
-        if(DEBUGFINAL):
-            if(LIMITEPASSOS>0):
-                if(passos>=LIMITEPASSOS):
-                    if(not(ERROINES)):
+        if config.debug_final:
+            if config.limit_steps > 0:
+                if config.steps >= config.limit_steps:
+                    if not (is_giving_error):
                         print("limite de passos")
                     break
-        leitura+=1
-        if(DEBUG):
-            print(str(index)+" "+str(tira))
-        if(leitura==len(programa)):
+        read_index += 1
+        if config.debug:
+            print(str(cursor) + " " + str(memory_tape))
+        if read_index == len(program_script):
             break
     print("")
-    return(tira)
+    return memory_tape
 
-DEBUG=0
-DEBUGFINAL=0
-LIMITEEXEC=10**4
-LIMITEVALOR=LIMITEEXEC
-LIMITEPASSOS=0
+
+def change_config_debug(config: Config, program_script: str) -> None:
+    if program_script == "config.debug ON":
+        config.debug = True
+        config.debug_final = True
+        print("config.debug ACTIVATED")
+        return
+    if program_script == "config.debug OFF":
+        config.debug = False
+        config.debug_final = False
+        print("config.debug DESACTIVATED")
+        return
+    if program_script == "config.debug FINAL ON":
+        config.debug_final = True
+        print("config.debug FINAL ACTIVATED")
+        return
+    if program_script == "config.debug FINAL OFF":
+        config.debug_final = False
+        print("config.debug FINAL DESACTIVATED")
+        return
+    if program_script.find("config.debug LIMIT STEPS ") != -1:
+        config.limit_steps = int(program_script[20:])
+        print(f"config.limit_steps={config.limit_steps}")
+        print("config.debug FINAL ACTIVATED")
+        config.debug_final = True
+        return
+    if program_script.find("config.debug LIMIT VALUE ") != -1:
+        config.limit_value = int(program_script[19:])
+        print(f"config.limit_value={config.limit_value}")
+        return
+    if program_script.find("config.debug LIMIT ") != -1:
+        config.limit_exec = int(program_script[13:])
+        print(f"config.limit_exec={config.limit_exec}")
+        return
+    if program_script.find("config.debug LIMITES ") != -1:
+        limits = int(program_script[14:])
+        config.limit_exec = limits
+        config.limit_steps = limits
+        config.limit_value = limits
+        print(f"config.limit_exec={config.limit_exec}")
+        print(f"config.limit_value={config.limit_value}")
+        print(f"config.limit_steps={config.limit_steps}")
+        print("config.debug FINAL ACTIVATED")
+        config.debug_final = True
+        return
+
+
+config = Config()
 while True:
-    isOut=2
-    passos=0
+    config.isOut = 2
+    config.steps = 0
     print("escreva o programa")
-    programa=input()
-    if(programa.find("DEBUG")!=-1):
-        if(programa=="DEBUG ON"):
-            DEBUG=1
-            DEBUGFINAL=True
-            print("DEBUG ATIVADO")
-        if(programa=="DEBUG OFF"):
-            DEBUG=0
-            DEBUGFINAL=False
-            print("DEBUG DESATIVADO")
-        if(programa=="DEBUG FINAL ON"):
-            DEBUGFINAL=True
-            print("DEBUG FINAL ATIVADO")
-        if(programa=="DEBUG FINAL OFF"):
-            DEBUGFINAL=False
-            print("DEBUG FINAL DESATIVADO")
-        if(programa.find("DEBUG LIMITE PASSOS ")!=-1):
-            LIMITEPASSOS=int(programa[20:])
-            print("LIMITEPASSOS="+str(LIMITEPASSOS))
-            print("DEBUG FINAL ATIVADO")
-            DEBUGFINAL=True
-        elif(programa.find("DEBUG LIMITE VALOR ")!=-1):
-            LIMITEVALOR=int(programa[19:])
-            print("LIMITEVALOR="+str(LIMITEVALOR))
-        elif(programa.find("DEBUG LIMITE ")!=-1):
-            LIMITEEXEC=int(programa[13:])
-            print("LIMITEEXEC="+str(LIMITEEXEC))
-        elif(programa.find("DEBUG LIMITES ")!=-1):
-            limites=int(programa[14:])
-            LIMITEEXEC=limites
-            LIMITEPASSOS=limites
-            LIMITEVALOR=limites
-            print("LIMITEEXEC="+str(LIMITEEXEC))
-            print("LIMITEVALOR="+str(LIMITEVALOR))
-            print("LIMITEPASSOS="+str(LIMITEPASSOS))
-            print("DEBUG FINAL ATIVADO")
-            DEBUGFINAL=True
+    program_script = input()
+    if program_script.find("config.debug") != -1:
+        change_config_debug(config, program_script)
         continue
-    elif(programa=="0"):
+    if program_script == "0":
         break
-    quantiaColcheteAberto=testaSeAcaba(programa)
-    tira=[0]
-    if(quantiaColcheteAberto==0):
-        tira=execucao(programa,tira)
-        if(DEBUGFINAL):
-            print("")
-            print(programa)
-            print(tira)
-            print("passos: "+str(passos))
-    elif(quantiaColcheteAberto<0):
-        print("falta "+str(-quantiaColcheteAberto)+" colchete: [")
-    else:
-        print("falta "+str(quantiaColcheteAberto)+" colchete: ]")
+    open_bracket_count = count_brackets(program_script)
+    memory_tape = [0]
+    if open_bracket_count < 0:
+        print("falta " + str(-open_bracket_count) + " colchete: [")
+        continue
+    if open_bracket_count > 0:
+        print("falta " + str(open_bracket_count) + " colchete: ]")
+        continue
+    memory_tape = cast(list[int], execute_program(program_script, memory_tape, config))
+    if config.debug_final:
+        print("")
+        print(program_script)
+        print(memory_tape)
+        print(f"steps: {config.steps}")
