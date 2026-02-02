f5ccd6bb53a0ffa5d603bf623ee023db203a7780
Camilla Alves <arcamilla42@gmail.com>
2026-02-02 13:54:39 -0300

    refactor: rename variables for clarity and consistency in automatic clicker script

--- automação/clickerTop.py
+++ automação/clickerTop.py
@@ -1,17 +1,19 @@
-import pyautogui, time
+import pyautogui
+import time
 
-# clicker automatico com mostragem de porcentagem
+# automatic clicker with percentage display
 
 time.sleep(5)
-limite = 10**3
-concluido = [0]
-apaga = 0
-for b in range(1, limite + 1):
+click_limit = 10**3
+percentage_achieved = [0]
+erase = 0
+for current_click in range(1, click_limit + 1):
     pyautogui.click(780, 460)
-    porcentagem = int(b / limite * 100)
-    if porcentagem not in concluido:
-        concluido.append(porcentagem)
-        mensagem = str(porcentagem) + "%"
-        print((apaga * "\b") + mensagem, end="", flush=True)
-        apaga = len(mensagem)
-apaga = input()  # só para pausar
+    percentage = int(current_click / click_limit * 100)
+    if percentage not in percentage_achieved:
+        percentage_achieved.append(percentage)
+        message = f"{percentage}%"
+        print((erase * "\b") + message, end="", flush=True)
+        erase = len(message)
+input()  # just to pause the console at the end
+print("\nDone!")
