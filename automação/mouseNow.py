7928ceafc5d73f9cf0c16c90f51b85a86a147b37
Camilla Alves <arcamilla42@gmail.com>
2026-02-02 14:05:08 -0300

    refactor: improve color display logic and streamline position output in mouse tracking script

--- automação/mouseNow.py
+++ automação/mouseNow.py
@@ -1,7 +1,11 @@
-#! python3
-# mouseNow.py - Displays the mouse cursor"s current position.
 import pyautogui
 import msvcrt
+
+
+def turn_color_to_string(color: tuple[int, int, int]) -> str:
+    color_list = ", ".join([f"{str(rgb_value).rjust(3)}" for rgb_value in color])
+    return f"RGB: ({color_list})"
+
 
 print("Press Ctrl-C to quit.")
 try:
@@ -10,12 +14,22 @@ try:
             key = msvcrt.getch()
             print(key)
         x, y = pyautogui.position()
-        positionStr = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
-        pixelColor = pyautogui.screenshot().getpixel((x, y))
-        positionStr += " RGB: (" + str(pixelColor[0]).rjust(3)
-        positionStr += ", " + str(pixelColor[1]).rjust(3)
-        positionStr += ", " + str(pixelColor[2]).rjust(3) + ")"
-        print(positionStr, end="")
-        print("\b" * len(positionStr), end="", flush=True)
+        color_captured = pyautogui.screenshot().getpixel((x, y))
+        if color_captured is None:
+            continue
+        pixel_color = (0, 0, 0)
+        if isinstance(color_captured, float) or isinstance(color_captured, int):
+            pixel_color = (
+                int(color_captured),
+                int(color_captured),
+                int(color_captured),
+            )
+        else:
+            if len(color_captured) <= 3:
+                continue
+            pixel_color = (color_captured[0], color_captured[1], color_captured[2])
+        text = f"X: {str(x).rjust(4)} Y: {str(y).rjust(4)} {turn_color_to_string(pixel_color)}"
+        print(text, end="")
+        print("\b" * len(text), end="", flush=True)
 except KeyboardInterrupt:
     print("\nDone.")
