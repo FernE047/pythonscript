faa13f2c6274f165262ebd545f34395d977cdf0d
Camilla Alves <arcamilla42@gmail.com>
2026-02-03 08:17:30 -0300

    refactor: enhance debugging capabilities and improve bracket counting logic in BrainF interpreter

--- computacao/BrainF___/BFInterpreterRollOver.py
+++ computacao/BrainF___/BFInterpreterRollOver.py
@@ -1,128 +1,146 @@
-def testaSeAcaba(programa):
-    loop=0
-    for elemento in programa:
-        if(elemento=="["):
-            loop+=1
-        elif(elemento=="]"):
-            loop-=1
-    return(loop)
+class Config:
+    def __init__(self, debug: bool = False, debug_final: bool = False) -> None:
+        self.debug = debug
+        self.debug_final = debug_final
+        self.steps = 0
+        self.isOut = 2
 
-def saiDoLoop(programa,leitura):
-    quantidadeAbertura=0
+    def update_debug(self, program_script: str) -> None:
+        if program_script == "DEBUG ON":
+            self.debug = True
+            self.debug_final = True
+            print("DEBUG ACTIVATED")
+            return
+        if program_script == "DEBUG OFF":
+            self.debug = False
+            self.debug_final = False
+            print("DEBUG DESACTIVATED")
+            return
+        if program_script == "DEBUG FINAL ON":
+            self.debug_final = True
+            print("DEBUG FINAL ACTIVATED")
+            return
+        if program_script == "DEBUG FINAL OFF":
+            self.debug_final = False
+            print("DEBUG FINAL DESACTIVATED")
+            return
+
+
+def count_open_bracket(program_script: str) -> int:
+    open_bracket_count = 0
+    for char in program_script:
+        if char == "[":
+            open_bracket_count += 1
+            continue
+        if char == "]":
+            open_bracket_count -= 1
+            continue
+    return open_bracket_count
+
+
+def find_end_of_loop(program_script: str, read_index: int) -> int:
+    open_bracket_count = 0
     while True:
-        leitura+=1
-        elemento=programa[leitura]
-        if(elemento=="["):
-            quantidadeAbertura+=1
-        if(elemento=="]"):
-            if(quantidadeAbertura==0):
-                break
-            else:
-                quantidadeAbertura-=1
-    return(leitura)
+        read_index += 1
+        try:
+            char = program_script[read_index]
+        except IndexError:
+            return read_index
+        if char == "[":
+            open_bracket_count += 1
+        if char == "]":
+            if open_bracket_count == 0:
+                return read_index
+            open_bracket_count -= 1
 
-def somaUm(num):
-    num+=1
-    if(num==256):
-        return(0)
-    return(num)
 
-def tiraUm(num):
-    num-=1
-    if(num==-1):
-        return(255)
-    return(num)
+def increment_and_wrap(current_value: int) -> int:
+    if current_value == 255:
+        return 0
+    return current_value + 1
 
-def execucao(programa,tira,leitura=0,index=0):
-    global DEBUG
-    global isOut
-    global passos
+
+def decrement_and_wrap(current_value: int) -> int:
+    if current_value == 0:
+        return 255
+    return current_value - 1
+
+
+def execute_script(program_script:str, memory_tape:list[int], config: Config, read_index:int=0, cursor:int=0) -> tuple[int,int]|list[int]:
     while True:
-        elemento=programa[leitura]
-        if(DEBUGFINAL):
-            passos+=1
-        if(DEBUG):
-            print(elemento,end=" ")
-        if(elemento=="+"):
-            tira[index]=somaUm(tira[index])
-        elif(elemento=="-"):
-            tira[index]=tiraUm(tira[index])
-        elif(elemento==">"):
-            index+=1
-            if(index>=len(tira)):
-                tira.append(0)
-        elif(elemento=="<"):
-            if(index==0):
-                tira=[0]+tira
+        char = program_script[read_index]
+        if config.debug_final:
+            config.steps += 1
+        if config.debug:
+            print(char, end=" ")
+        if char == "+":
+            memory_tape[cursor] = increment_and_wrap(memory_tape[cursor])
+        elif char == "-":
+            memory_tape[cursor] = decrement_and_wrap(memory_tape[cursor])
+        elif char == ">":
+            cursor += 1
+            if cursor >= len(memory_tape):
+                memory_tape.append(0)
+        elif char == "<":
+            if cursor == 0:
+                memory_tape = [0] + memory_tape
             else:
-                index-=1
-        elif(elemento==","):
-            isOut=0
-            tira[index]=int(input("\nentrada:"))
-        elif(elemento=="."):
-            if(isOut==2):
-                isOut=1
-                print("saida:"+str(tira[index]),end="")
-            elif(isOut==1):
-                print(" "+str(tira[index]),end="")
+                cursor -= 1
+        elif char == ",":
+            config.isOut = 0
+            memory_tape[cursor] = int(input("\ninput:"))
+        elif char == ".":
+            if config.isOut == 2:
+                config.isOut = 1
+                print(f"output: {memory_tape[cursor]}", end="")
+            elif config.isOut == 1:
+                print(f" {memory_tape[cursor]}", end="")
             else:
-                isOut=1
-                print("\nsaida:"+str(tira[index]),end="")
-        elif(elemento=="["):
-            if(DEBUG):
+                config.isOut = 1
+                print(f"\noutput: {memory_tape[cursor]}", end="")
+        elif char == "[":
+            if config.debug:
                 print("")
-            if(tira[index]):
-                leituraInicial=leitura+1
-                while(tira[index]):
-                    index,leitura=execucao(programa,tira,leituraInicial,index)
+            if memory_tape[cursor]:
+                leituraInicial = read_index + 1
+                while memory_tape[cursor]:
+                    cursor, read_index = execute_script(
+                        program_script, memory_tape, config, leituraInicial, cursor
+                    )
             else:
-                leitura=saiDoLoop(programa,leitura)
-        elif(elemento=="]"):
-            if(DEBUG):
+                read_index = find_end_of_loop(program_script, read_index)
+        elif char == "]":
+            if config.debug:
                 print("]")
-            return((index,leitura))
-        leitura+=1
-        if(DEBUG):
-            print(str(index)+" "+str(tira))
-        if(leitura==len(programa)):
+            return (cursor, read_index)
+        read_index += 1
+        if config.debug:
+            print(f"{cursor} {memory_tape}")
+        if read_index == len(program_script):
             break
     print("")
-    return(tira)
+    return memory_tape
 
-DEBUG=0
-DEBUGFINAL=0
+
+config = Config()
 while True:
-    isOut=2
-    passos=0
-    print("escreva o programa")
-    programa=input()
-    if(programa=="0"):
+    print("enter the brainfuck program")
+    programa = input()
+    if programa == "0":
         break
-    if(programa=="DEBUG ON"):
-        DEBUG=1
-        DEBUGFINAL=True
-        print("DEBUG ATIVADO")
-    if(programa=="DEBUG OFF"):
-        DEBUG=0
-        DEBUGFINAL=False
-        print("DEBUG DESATIVADO")
-    if(programa=="DEBUG FINAL ON"):
-        DEBUGFINAL=True
-        print("DEBUG FINAL ATIVADO")
-    if(programa=="DEBUG FINAL OFF"):
-        DEBUGFINAL=False
-        print("DEBUG FINAL DESATIVADO")
-    if(programa.find("DEBUG")!=-1):
+    if programa.find("DEBUG") != -1:
+        config.update_debug(programa)
         continue
-    quantiaColcheteAberto=testaSeAcaba(programa)
-    if(quantiaColcheteAberto==0):
-        tira=execucao(programa,[0])
-        if(DEBUGFINAL):
+    open_bracket_count = count_open_bracket(programa)
+    if open_bracket_count == 0:
+        memory_tape = execute_script(programa, [0], config)
+        if config.debug_final:
             print("")
             print(programa)
-            print(tira)
-            print("passos: "+str(passos))
-    elif(quantiaColcheteAberto<0):
-        print("falta "+str(-quantiaColcheteAberto)+" colchete: [")
-    else:
-        print("falta "+str(quantiaColcheteAberto)+" colchete: ]")
+            print(memory_tape)
+            print(f"steps: {config.steps}")
+        continue
+    if open_bracket_count < 0:
+        print(f"missing {-open_bracket_count} bracket: [")
+        continue
+    print(f"missing {open_bracket_count} bracket: ]")
