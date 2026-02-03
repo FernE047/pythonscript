040dbfeb44dd1ede1b552d3ee2d08fb78c65d93d
Camilla Alves <arcamilla42@gmail.com>
2026-02-03 09:22:48 -0300

    refactor: rename functions and improve readability in Java batch file generation

--- compiladorJava.py
+++ compiladorJava.py
@@ -1,42 +1,46 @@
 import os
 import subprocess
 
+DIRECTORY = "C:/JavaProgs"
 
-def hasDotJavaInside(pasta):
-    for arquivo in [diretorio + "\\" + arq for arq in os.listdir(pasta)]:
-        if arquivo.find(".java") != -1:
+
+def contains_java_file(folder: str) -> bool:
+    for file_name in [f"{DIRECTORY}/{arq}" for arq in os.listdir(folder)]:
+        if file_name.find(".java") != -1:
             return True
     return False
 
 
-def fazBat(nome):
-    nomeProject = nome[nome.rfind("\\") + 1 : nome.rfind(".")]
-    nomeBat = "C:\\JavaProgs\\" + nomeProject + ".bat"
-    print(nomeBat)
-    nomeJava = pasta + "\\" + nomeProject
-    with open(nomeBat, "w",encoding="utf-8") as file:
-        file.write("@echo off\njava " + nomeJava + " %*")  # \npause")
+def create_batch_file(file_name: str) -> None:
+    project_name = file_name[file_name.rfind("/") + 1 : file_name.rfind(".")]
+    batch_name = f"{DIRECTORY}/{project_name}.bat"
+    print(batch_name)
+    java_name = f"{file_name}/{project_name}"
+    with open(batch_name, "w", encoding="utf-8") as file:
+        file.write(f"@echo off\njava {java_name} %*")
 
 
+def create_batch_project(project_folder: str) -> None:
+    project_name = project_folder[project_folder.rfind("/") + 1 :]
+    batch_name = f"{DIRECTORY}/{project_name}.bat"
+    print(batch_name)
+    java_name = f"{project_folder}/teste.Teste"
+    with open(batch_name, "w", encoding="utf-8") as file:
+        file.write(f"@echo off\njava {java_name} %*")
 
-def fazBatProject(nome):
-    nomeProject = nome[nome.rfind("\\") + 1 :]
-    nomeBat = "C:\\JavaProgs\\" + nomeProject + ".bat"
-    print(nomeBat)
-    nomeJava = pasta + "\\teste.Teste"
-    with open(nomeBat, "w",encoding="utf-8") as file:
-        file.write("@echo off\njava " + nomeJava + " %*")  # \npause()
 
-diretorio = "C:\\JavaProgs"
-for pasta in [diretorio + "\\" + folder for folder in os.listdir(diretorio)]:
-    if hasDotJavaInside(pasta):
-        print("javac " + pasta + "\\*.java")
-        subprocess.Popen("javac " + pasta + "\\*.java", shell=True)
-        fazBat(pasta)
-    else:
-        print("dir /s /B " + pasta + "\\*.java > " + pasta + "\\sources.txt")
-        subprocess.Popen(
-            "dir /s /B " + pasta + "\\*.java > " + pasta + "\\sources.txt", shell=True
-        )
-        subprocess.Popen("javac @" + pasta + "\\sources.txt", shell=True)
-        fazBatProject(pasta)
+def run_command(command: str) -> None:
+    print(command)
+    subprocess.Popen(command, shell=True)
+
+
+folders = os.listdir(DIRECTORY)
+folder_list = [f"{DIRECTORY}/{folder}" for folder in folders]
+for folder in folder_list:
+    if contains_java_file(folder):
+        run_command(f"javac {folder}/*.java")
+        create_batch_file(folder)
+        continue
+    run_command(f"dir /s /B {folder}/*.java > {folder}/sources.txt")
+    run_command(f"javac @{folder}/sources.txt")
+    create_batch_project(folder)
