190d317d8474a0c80dcd5cdcf6159fe3736fe10e
Camilla Alves <arcamilla42@gmail.com>
2026-02-03 09:34:17 -0300

    refactor: improve function signature and variable naming in Ackerman function

--- computacao/ackerman.py
+++ computacao/ackerman.py
@@ -1,19 +1,20 @@
-def ackerman(m,n):
-    global nivel
-    nivel+=1
-    print(f"ackerman({m},{n})\tnivel={nivel}")
-    if(m<=0):
-        return(n+1)
-    if((m>0)and(n<=0)):
-        retorno=ackerman(m-1,1)
-        nivel-=1
-        return(retorno)
-    if((m>0)and(n>0)):
-        retorno=ackerman(m-1,ackerman(m,n-1))
-        nivel-=2
-        return(retorno)
+def ackerman(m: int, n: int) -> int:
+    global level
+    level += 1
+    print(f"ackerman({m},{n})\tlevel={level}")
+    if m <= 0:
+        return n + 1
+    if (m > 0) and (n <= 0):
+        result_value = ackerman(m - 1, 1)
+        level -= 1
+        return result_value
+    if (m > 0) and (n > 0):
+        result_value = ackerman(m - 1, ackerman(m, n - 1))
+        level -= 2
+        return result_value
     print("erro")
-    return(n-1)
+    return n - 1
 
-nivel=0
-print(ackerman(3,10))
+
+level = 0
+print(ackerman(3, 10))
