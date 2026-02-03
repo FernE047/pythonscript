95d3cb799737a390683bce7a8db3c5e939cd0059
Camilla Alves <arcamilla42@gmail.com>
2026-02-03 08:47:11 -0300

    refactor: encapsulate global variables in Config class and improve BrainF code generation logic

--- computacao/BrainF___/BFPossibilities.py
+++ computacao/BrainF___/BFPossibilities.py
@@ -1,182 +1,205 @@
-def transformaEmPrograma(info):
-    global ELEMENTOS
-    idPrograma,tamanho=info
-    lista=[]
-    while(idPrograma>5):
-        elemento=idPrograma%6
-        idPrograma=idPrograma//6
-        lista.append(ELEMENTOS[elemento])
-    lista.append(ELEMENTOS[idPrograma])
-    while(len(lista)<tamanho):
-        lista.append(ELEMENTOS[0])
-    programa="".join(lista)
-    return(programa)
-    
+from typing import cast
 
-def fazPrograma(idPrograma):
-    idOficial=viraId(idPrograma)
-    programa=transformaEmPrograma(idOficial)
-    return(programa)
 
-def viraId(num):
-    tamanho=1
-    heuristicaValor=0
-    idOficial=0
+class Config:
+    # this code used to have a lot of global variables, now they are encapsulated in this class
+    def __init__(self) -> None:
+        self.debug = False
+        self.debug_final = False
+        self.limit_exec = 10000
+        self.limit_value = 10000
+        self.limit_steps = 10000
+        self.steps = 0
+        self.enable_print = False
+        self.is_giving_error = False
+        self.valid_script_count = 0
+        self.script_id = 0
+
+
+def generate_bf_code(info: tuple[int, int]) -> str:
+    available_chars = ["+", "-", "<", ">", "[", "]"]
+    id_script, size = info
+    brainfuck_instructions: list[str] = []
+    while id_script > 5:
+        char = id_script % 6
+        id_script = id_script // 6
+        brainfuck_instructions.append(available_chars[char])
+    brainfuck_instructions.append(available_chars[id_script])
+    while len(brainfuck_instructions) < size:
+        brainfuck_instructions.append(available_chars[0])
+    program_script = "".join(brainfuck_instructions)
+    return program_script
+
+
+def generate_code(config: Config) -> str:
+    id_official = convert_id(config.script_id)
+    program_script = generate_bf_code(id_official)
+    return program_script
+
+
+def convert_id(number: int) -> tuple[int, int]:
+    size = 1
+    heuristic_value = 0
+    official_id = 0
     while True:
-        total=6**tamanho
-        heuristicaValor+=total
-        if(num<heuristicaValor):
-            return((num-idOficial,tamanho))
-        else:
-            idOficial+=total
-        tamanho+=1
+        total = 6**size
+        heuristic_value += total
+        if number < heuristic_value:
+            return (number - official_id, size)
+        official_id += total
+        size += 1
 
-def mostraDados(dicionario):
-    global programaValido
-    global idPrograma
+
+def display_results(result_dictionary: dict[int, str], config: Config) -> None:
     print("")
-    for a in range(min(dicionario),max(dicionario)+1):
-        if(a in dicionario.keys()):
-            print(programaValido,end="/")
-            print(idPrograma,end="\t")
-            print(a,end="\t")
-            print(dicionario[a])
+    for key_number in range(min(result_dictionary), max(result_dictionary) + 1):
+        if key_number in result_dictionary.keys():
+            print(config.valid_script_count, end="/")
+            print(config.script_id, end="\t")
+            print(key_number, end="\t")
+            print(result_dictionary[key_number])
 
-def testaSeAcaba(programa):
-    loop=0
-    for elemento in programa:
-        if(elemento=="["):
-            loop+=1
-        elif(elemento=="]"):
-            if(loop==0):
-                return(-1)
-            loop-=1
-    return(loop)
 
-def saiDoLoop(programa,leitura):
-    quantidadeAbertura=0
+def check_bracket_balance(program_script: str) -> int:
+    open_bracket_count = 0
+    for char in program_script:
+        if char == "[":
+            open_bracket_count += 1
+            continue
+        if char == "]":
+            if open_bracket_count == 0:
+                return -1
+            open_bracket_count -= 1
+    return open_bracket_count
+
+
+def find_closing_bracket(program_script: str, read_index: int) -> int:
+    open_bracket_count = 0
     while True:
-        leitura+=1
-        elemento=programa[leitura]
-        if(elemento=="["):
-            quantidadeAbertura+=1
-        if(elemento=="]"):
-            if(quantidadeAbertura==0):
-                break
-            else:
-                quantidadeAbertura-=1
-    return(leitura)
+        read_index += 1
+        char = program_script[read_index]
+        if char == "[":
+            open_bracket_count += 1
+            continue
+        if char == "]":
+            if open_bracket_count == 0:
+                return read_index
+            open_bracket_count -= 1
 
-def ehErro(tira,index):
-    global passos
-    global LIMITEEXEC
-    global LIMITEPASSOS
-    global LIMITEVALOR
-    global ERROINES
-    global impressao
+
+def test_errors(memory_tape: list[int], cursor: int, config: Config) -> bool:
     try:
-        if(len(tira)>=LIMITEEXEC):
-            if(not(ERROINES)):
-                if(impressao):
-                    print("limite de execução",end="\t")
-            return(True)
-        if((tira[index]>=LIMITEVALOR)or(tira[index]<=-LIMITEVALOR)):
-            if(not(ERROINES)):
-                if(impressao):
-                    print("limite de valor excedido pelo indice "+str(index)+" : "+str(tira[index]),end="\t")
-            return(True)
-        if(LIMITEPASSOS>0):
-            if(passos>=LIMITEPASSOS):
-                if(not(ERROINES)):
-                    if(impressao):
-                        print("limite de passos",end="\t")
-                return(True)
-    except:
-        return(True)
-    return(False)
+        if len(memory_tape) >= config.limit_exec:
+            if not config.is_giving_error:
+                if config.enable_print:
+                    print("execution limit exceeded", end="\t")
+            return True
+        if (memory_tape[cursor] >= config.limit_value) or (
+            memory_tape[cursor] <= -config.limit_value
+        ):
+            if not config.is_giving_error:
+                if config.enable_print:
+                    print(
+                        f"value limit exceeded at index {cursor} : {memory_tape[cursor]}",
+                        end="\t",
+                    )
+            return True
+        if config.limit_steps > 0:
+            if config.steps >= config.limit_steps:
+                if not config.is_giving_error:
+                    if config.enable_print:
+                        print("step limit exceeded", end="\t")
+                return True
+    except Exception as _:
+        print("memory access error", end="\t")
+        return True
+    return False
 
-def execucao(programa,tira,leitura=0,index=0):
-    global DEBUGFINAL
-    global passos
-    global LIMITEEXEC
-    global LIMITEPASSOS
-    global LIMITEVALOR
-    global ERROINES
-    ERROINES=False
+
+def execute_bf(
+    program_script: str,
+    memory_tape: tuple[int, int, list[int]] | list[int],
+    config: Config,
+    read_index: int = 0,
+    cursor: int = 0,
+) -> tuple[int, int, list[int]] | list[int]:
+    assert isinstance(memory_tape, list)
+    config.is_giving_error = False
     while True:
-        if(ehErro(tira,index)):
+        if test_errors(memory_tape, cursor, config):
             break
-        elemento=programa[leitura]
-        if(DEBUGFINAL):
-            passos+=1
-        if(elemento=="+"):
-            tira[index]+=1
-        elif(elemento=="-"):
-            tira[index]-=1
-        elif(elemento==">"):
-            index+=1
-            if(index>=len(tira)):
-                tira.append(0)
-        elif(elemento=="<"):
-            if(index==0):
-                tira=[0]+tira
+        char = program_script[read_index]
+        if config.debug_final:
+            config.steps += 1
+        if char == "+":
+            memory_tape[cursor] += 1
+        elif char == "-":
+            memory_tape[cursor] -= 1
+        elif char == ">":
+            cursor += 1
+            if cursor >= len(memory_tape):
+                memory_tape.append(0)
+        elif char == "<":
+            if cursor == 0:
+                memory_tape = [0] + memory_tape
             else:
-                index-=1
-        elif(elemento=="["):
-            if(tira[index]):
-                leituraInicial=leitura+1
+                cursor -= 1
+        elif char == "[":
+            if memory_tape[cursor]:
+                leituraInicial = read_index + 1
                 try:
-                    while(tira[index]):
-                        index,leitura,tira=execucao(programa,tira,leituraInicial,index)
-                except:
-                    ERROINES=True
+                    while memory_tape[cursor]:
+                        cursor, read_index, memory_tape = cast(
+                            tuple[int, int, list[int]],
+                            execute_bf(
+                                program_script,
+                                memory_tape,
+                                config,
+                                leituraInicial,
+                                cursor,
+                            ),
+                        )
+                except Exception as _:
+                    config.is_giving_error = True
             else:
-                leitura=saiDoLoop(programa,leitura)
-        elif(elemento=="]"):
-            return((index,leitura,tira))
-        leitura+=1
-        if(ehErro(tira,index)):
+                read_index = find_closing_bracket(program_script, read_index)
+        elif char == "]":
+            return (cursor, read_index, memory_tape)
+        read_index += 1
+        if test_errors(memory_tape, cursor, config):
             break
-        if(leitura==len(programa)):
+        if read_index == len(program_script):
             break
-    return(tira)
+    return memory_tape
 
-DEBUGFINAL=True
-LIMITEEXEC=10**4
-LIMITEVALOR=LIMITEEXEC
-LIMITEPASSOS=LIMITEEXEC
-impressao=False
-ERROINES=False
-idPrograma=0
-ELEMENTOS=["+","-",">","<","[","]"]
-menoresPassos={}
-jaEncontrados=[]
-menoresTamanho={}
-programaValido=0
-potLimite=10
-while (programaValido<=10**potLimite):
-    passos=0
-    programa=fazPrograma(idPrograma)
-    quantiaColcheteAberto=testaSeAcaba(programa)
-    if(quantiaColcheteAberto==0):
-        tira=[0]
-        tira=execucao(programa,tira)
-        if(impressao):
-            print(programaValido,end="/")
-            print(idPrograma,end="\t")
-            print(programa,end="\t")
-            print(tira,end="\t")
-            print("passos: "+str(passos))
-        if isinstance(tira,list):
-            for elemento in tira:
-                if(passos==LIMITEEXEC):
+
+config = Config()
+found_results: list[int] = []
+optimal_step_count: dict[int, str] = {}
+potLimite = 10
+while config.valid_script_count <= 10**potLimite:
+    config.steps = 0
+    programa = generate_code(config)
+    quantiaColcheteAberto = check_bracket_balance(programa)
+    if quantiaColcheteAberto == 0:
+        memory_tape: tuple[int, int, list[int]] | list[int] = [0]
+        memory_tape = execute_bf(programa, memory_tape, config)
+        if config.enable_print:
+            print(config.valid_script_count, end="/")
+            print(config.script_id, end="\t")
+            print(programa, end="\t")
+            print(memory_tape, end="\t")
+            print("passos: " + str(config.steps))
+        if isinstance(memory_tape, list):
+            for result_output in memory_tape:
+                if config.steps == config.limit_exec:
                     break
-                if elemento not in jaEncontrados:
-                    jaEncontrados.append(elemento)
-                    menoresTamanho[elemento]=programa
-        programaValido+=1
-    idPrograma+=1
-    if(idPrograma%(7*10**(potLimite-2))==0):
-        mostraDados(menoresTamanho)
-for a in range(min(menoresTamanho),max(menoresTamanho)+1):
-    mostraDados(menoresTamanho)
+                if result_output not in found_results:
+                    found_results.append(result_output)
+                    optimal_step_count[result_output] = programa
+        config.valid_script_count += 1
+    config.script_id += 1
+    if config.script_id % (7 * 10 ** (potLimite - 2)) == 0:
+        display_results(optimal_step_count, config)
+for a in range(min(optimal_step_count), max(optimal_step_count) + 1):
+    display_results(optimal_step_count, config)
