5889018c4a29c0542c859895ece31355f27f2608
Camilla Alves <arcamilla42@gmail.com>
2026-02-03 09:55:01 -0300

    refactor: improve variable naming for clarity in leituraTeste.py

--- computacao/leituraTeste.py
+++ computacao/leituraTeste.py
@@ -1,9 +1,11 @@
+# first time reading and writing files in python
+
 with open("vazio.txt", "r", encoding="utf-8") as file:
     print(file)
-    linha = file.readline()
-    while linha:
-        print(linha)
-        linha = file.readline()
+    line = file.readline()
+    while line:
+        print(line)
+        line = file.readline()
 with open("escritaTeste.txt", "w", encoding="utf-8") as file:
     file.write("hello world")
     file.write("\n")
