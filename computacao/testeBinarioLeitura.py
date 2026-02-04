aa06d17e901d9b2b884d0bd633e66dc32f544e9b
Camilla Alves <arcamilla42@gmail.com>
2026-02-04 08:16:20 -0300

    refactor: remove unnecessary encoding parameters and streamline byte processing in testeBinarioLeitura

--- computacao/testeBinarioLeitura.py
+++ computacao/testeBinarioLeitura.py
@@ -4,19 +4,19 @@ import os
 def main() -> None:
     diretorio = os.getcwd()
     imagens = [os.path.join(diretorio, imagem) for imagem in os.listdir()]
-    with open(os.path.join(diretorio, "copia.py"), "wb", encoding="utf-8") as novoArquivo:
-        with open(imagens[0], "rb", encoding="utf-8") as arquivo:
+    with open(os.path.join(diretorio, "copia.py"), "wb") as novoArquivo:
+        with open(imagens[0], "rb") as arquivo:
             novoArquivo.write(arquivo.read())
-        with open(imagens[0], "rb", encoding="utf-8") as arquivo:
+        with open(imagens[0], "rb") as arquivo:
             byte = arquivo.read(1)
             while byte:
-                b = novoArquivo.write(byte)
-                a = int.from_bytes(byte, byteorder="big")
-                a = bin(a)
-                a = a.lstrip("0b")
-                while len(a) < 8:
-                    a = "0" + a
-                print(a)
+                novoArquivo.write(byte)
+                byte_as_integer = int.from_bytes(byte, byteorder="big")
+                byte_as_char = bin(byte_as_integer)
+                byte_as_char = byte_as_char.lstrip("0b")
+                while len(byte_as_char) < 8:
+                    byte_as_char = "0" + byte_as_char
+                print(byte_as_char)
                 byte = arquivo.read(1)
 
 
