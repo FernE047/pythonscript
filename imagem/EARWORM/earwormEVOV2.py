94493b1df4d98e454eacc41d5d8b556e7f7af741
Camilla Alves <arcamilla42@gmail.com>
2026-02-04 14:29:37 -0300

    refactor: replace old image generation functions with streamlined lyrics processing and image creation logic

--- imagem/EARWORM/earwormEVOV2.py
+++ imagem/EARWORM/earwormEVOV2.py
@@ -1,40 +1,40 @@
-from typing import Callable
 from PIL import Image
 import os
 
+RED = (255, 0, 0, 255)
+BLACK = (0, 0, 0, 255)
 
-def cadaMusicaFaca(func: Callable[[tuple[str, str]], Image.Image]) -> None:
-    pastaMusicas = "C:\\pythonscript\\imagem\\EARWORM\\musicas\\"
-    pastaSalvar = "C:\\pythonscript\\imagem\\EARWORM\\imagens\\"
-    if not os.path.exists(pastaSalvar):
-        os.makedirs(pastaSalvar)
-    for nomeArquivo in os.listdir(pastaMusicas):
-        if nomeArquivo.endswith(".txt"):
-            caminhoCompleto = os.path.join(pastaMusicas, nomeArquivo)
-            with open(caminhoCompleto, "r", encoding="utf-8") as f:
-                musica = f.read()
-            imagem = func((nomeArquivo[:-4], musica))
-            imagem.save(os.path.join(pastaSalvar, f"{nomeArquivo[:-4]}.png"))
-
-
-def fazImagem(info):
-    _, musica = info
-    musicaSeparada = musica.split(" ")
-    quantPalavras = len(musicaSeparada)
-    print("quantidade de palavras: " + str(quantPalavras) + "\n")
-    if quantPalavras:
-        imagem = Image.new("RGBA", (quantPalavras, quantPalavras), (0, 0, 0, 255))
-        for coordX in range(quantPalavras):
-            for coordY in range(quantPalavras):
-                if musicaSeparada[coordX] == musicaSeparada[coordY]:
-                    imagem.putpixel((coordX, coordY), (255, 0, 0, 255))
-        return imagem
+def generate_image(lyrics:str) -> Image.Image| None:
+    words = lyrics.split(" ")
+    word_counter = len(words)
+    print(f"Word Count: {word_counter}\n")
+    if not word_counter:
+        print("No lyrics found\n")
+        return None
+    image = Image.new("RGBA", (word_counter, word_counter), BLACK)
+    for x in range(word_counter):
+        for y in range(word_counter):
+            if words[x] == words[y]:
+                image.putpixel((x, y), RED)
+    return image
 
 
 
 def main() -> None:
-    cadaMusicaFaca(fazImagem)
-
+    lyrics_folder = "./data/lyrics/"
+    image_folder = "./data/images/"
+    if not os.path.exists(image_folder):
+        os.makedirs(image_folder)
+    for filename in os.listdir(lyrics_folder):
+        if not filename.endswith(".txt"):
+            continue
+        full_path = os.path.join(lyrics_folder, filename)
+        with open(full_path, "r", encoding="utf-8") as f:
+            lyrics = f.read()
+        image = generate_image(lyrics)
+        if image is not None:
+            image_path = os.path.join(image_folder, f"{filename[:-4]}.png")
+            image.save(image_path)
 
 if __name__ == "__main__":
     main()
