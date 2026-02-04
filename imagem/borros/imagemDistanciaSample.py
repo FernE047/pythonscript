30edc9699d2cf05856db7f91b4344358f45f1f61
Camilla Alves <arcamilla42@gmail.com>
2026-02-04 10:09:40 -0300

    refactor: enhance distance calculation and improve image generation logic in fazImagem function

--- imagem/borros/imagemDistanciaSample.py
+++ imagem/borros/imagemDistanciaSample.py
@@ -1,20 +1,33 @@
 from PIL import Image
 from colorsys import hsv_to_rgb
 
-def fazImagem(pontoFinal,nome):
-    image = Image.new("RGBA",(256,256),(0,0,100))
-    distancia = lambda ponto1,ponto2:((ponto1[0]-ponto2[0])**2+(ponto1[1]-ponto2[1])**2)**0.5
-    D = distancia((0,0),pontoFinal)
+CoordData = tuple[int, int]
+
+
+def get_distance(point_a: CoordData, point_b: CoordData) -> float:
+    distance_a = point_a[0] - point_b[0]
+    distance_b = point_a[1] - point_b[1]
+    distance = (distance_a**2 + distance_b**2) ** 0.5
+    return distance
+
+
+def fazImagem(focus_point: CoordData, file_name: str) -> None:
+    image = Image.new("RGBA", (256, 256), (0, 0, 100))
+    distance_from_focus = get_distance((0, 0), focus_point)
     for x in range(256):
         for y in range(256):
-            image.putpixel((x,y),tuple([int(a) for a in hsv_to_rgb(0,0,distancia((x,y),pontoFinal)/D*255)]))
-    image.save(nome)
+            current_distance = get_distance((x, y), focus_point)
+            brightness = current_distance / distance_from_focus * 255
+            rgb_values = hsv_to_rgb(0, 0, brightness)
+            pixel_rgb = tuple([int(color_component) for color_component in rgb_values])
+            image.putpixel((x, y), pixel_rgb)
+    image.save(file_name)
 
 
 def main() -> None:
-    fazImagem((255,255),"C:\\Users\\Programador\\Desktop\\pic1.png")
-    fazImagem((127,127),"C:\\Users\\Programador\\Desktop\\pic2.png")
-    fazImagem((185,220),"C:\\Users\\Programador\\Desktop\\pic3.png")
+    fazImagem((255, 255), "pic1.png")
+    fazImagem((127, 127), "pic2.png")
+    fazImagem((185, 220), "pic3.png")
 
 
 if __name__ == "__main__":
