a116b433038270acf35393d6039ba40bd28ed5c0
Camilla Alves <arcamilla42@gmail.com>
2026-02-05 16:45:53 -0300

    refactor: simplify image loading and processing logic for improved clarity and efficiency

--- imagem/evoluiPokemon/recolor.py
+++ imagem/evoluiPokemon/recolor.py
@@ -1,34 +1,42 @@
 from PIL import Image
-import os
+
+SOURCE_IMAGE = "./source.png"
+TARGET_IMAGE = "./target.png"
+SOURCE_IMAGE_COLORED = "./source_colored.png"
+TARGET_IMAGE_COLORED = "./target_colored.png"
+CONFIG_FILE = "./config.txt"
 
 
 def main() -> None:
-    directory = "C:\\pythonscript\\imagem\\evoluiPokemon\\"
-
-    imagemInicial = Image.open(directory + "inicial.png")
-    imagemFinal = Image.open(directory + "final.png")
+    source_image = Image.open(SOURCE_IMAGE)
+    target_image = Image.open(TARGET_IMAGE)
     print("recolorindo...")
-
-    inicialRecolor = Image.open(directory + "inicial.png")
-    finalRecolor = Image.open(directory + "final.png")
-    with open(directory + "config.txt", "r", encoding="utf-8") as file:
-        linha = file.readline()
-        while linha:
-            coordInicial, coordFinal = [
-                tuple([int(b) for b in coord.split(",")]) for coord in linha.split(" ")
+    source_image_colored = source_image.copy()
+    target_image_colored = target_image.copy()
+    with open(CONFIG_FILE, "r", encoding="utf-8") as file:
+        line = file.readline()
+        while line:
+            coords: list[tuple[int, ...]] = [
+                tuple([int(b) for b in coord.split(",")]) for coord in line.split(" ")
             ]
-            pixelFinal = imagemFinal.getpixel(coordFinal)
-            pixelInicial = imagemInicial.getpixel(coordInicial)
-            inicialRecolor.putpixel(coordInicial, pixelFinal)
-            finalRecolor.putpixel(coordFinal, pixelInicial)
-            linha = file.readline()
-
-    inicialRecolor.save(directory + "inicialColored.png")
-    finalRecolor.save(directory + "finalColored.png")
-    inicialRecolor.close()
-    finalRecolor.close()
-    imagemInicial.close()
-    imagemFinal.close()
+            coord_source, coord_target = coords
+            if len(coord_source) != 2 or len(coord_target) != 2:
+                raise ValueError(f"Invalid coordinate format: {line}")
+            pixel_target = target_image.getpixel(coord_target)
+            if pixel_target is None:
+                raise ValueError(f"Pixel not found at coordinate: {coord_target}")
+            pixel_source = source_image.getpixel(coord_source)
+            if pixel_source is None:
+                raise ValueError(f"Pixel not found at coordinate: {coord_source}")
+            source_image_colored.putpixel(coord_source, pixel_target)
+            target_image_colored.putpixel(coord_target, pixel_source)
+            line = file.readline()
+    source_image_colored.save(SOURCE_IMAGE_COLORED)
+    target_image_colored.save(TARGET_IMAGE_COLORED)
+    source_image_colored.close()
+    target_image_colored.close()
+    source_image.close()
+    target_image.close()
     print("R E C O L O R I D O   :D")
 
 
