430b6718f89ee729e3de6d3b8e9a2de0138eadde
Camilla Alves <arcamilla42@gmail.com>
2026-02-06 09:28:52 -0300

    refactor: replace pixel comparison logic with dedicated functions for clarity and maintainability

--- imagem/imagemLiner/HSV/linerImages.py
+++ imagem/imagemLiner/HSV/linerImages.py
@@ -1,45 +1,62 @@
 from PIL import Image
 from colorsys import rgb_to_hsv
 
+BACKGROUND_COLOR = (255, 255, 255, 255)
+HIGHLIGHT_COLOR = (0, 0, 0, 255)
+VALUE_THRESHOLD_MULTIPLIER = 10
+OUTPUT_IMAGE_COUNT = 5
+INPUT_IMAGE_COUNT = 1
 
-def compara(pixelA, pixelB):
-    return abs(
-        rgb_to_hsv(pixelA[0], pixelA[1], pixelA[2])[2]
-        - rgb_to_hsv(pixelB[0], pixelB[1], pixelB[2])[2]
-    )
 
+def get_pixel(imagem: Image.Image, x: int, y: int) -> tuple[int, ...]:
+    pixel = imagem.getpixel((x, y))
+    if pixel is None:
+        return BACKGROUND_COLOR
+    if not isinstance(pixel, tuple):
+        pixel_int = int(pixel)
+        return (pixel_int, pixel_int, pixel_int, 255)
+    return pixel
+
+
+def get_hsv_value_difference(
+    pixel_a: tuple[int, ...], pixel_b: tuple[int, ...]
+) -> float:
+    hsv_a = rgb_to_hsv(pixel_a[0], pixel_a[1], pixel_a[2])
+    hsv_b = rgb_to_hsv(pixel_b[0], pixel_b[1], pixel_b[2])
+    return abs(hsv_a[2] - hsv_b[2])
 
 
 def main() -> None:
-    nomeOpen = "A{0:03d}0.jpg"
-    nomeSave = "A{0:03d}{1:01d}.png"
-    for a in range(1):
-        imagem = Image.open(nomeOpen.format(a))
-        altura, largura = imagem.size
-        print(
-            "\nimagem " + nomeOpen.format(a) + " tamanho: " + str(imagem.size), end="\n\n"
-        )
-        for b in range(1, 6):
-            imagemTransform = Image.new("RGBA", imagem.size, (255, 255, 255, 255))
-            for y in range(altura):
-                for x in range(largura):
-                    pixelAtual = imagem.getpixel((y, x))
-                    arredores = []
+    for input_index in range(INPUT_IMAGE_COUNT):
+        input_name = f"A{input_index:03d}0.jpg"
+        imagem = Image.open(input_name)
+        width, height = imagem.size
+        print(f"\nimage {input_name} size: {imagem.size}\n")
+        for output_index in range(1, OUTPUT_IMAGE_COUNT + 1):
+            output_image = Image.new("RGBA", imagem.size, BACKGROUND_COLOR)
+            for y in range(height):
+                for x in range(width):
+                    current_pixel = get_pixel(imagem, x, y)
+                    neighbor_pixels: list[tuple[int, ...]] = []
                     if x > 0:
-                        arredores.append(imagem.getpixel((y, x - 1)))
-                    if x < largura - 1:
-                        arredores.append(imagem.getpixel((y, x + 1)))
+                        neighbor_pixels.append(get_pixel(imagem, x - 1, y))
+                    if x < width - 1:
+                        neighbor_pixels.append(get_pixel(imagem, x + 1, y))
                     if y > 0:
-                        arredores.append(imagem.getpixel((y - 1, x)))
-                    if y < altura - 1:
-                        arredores.append(imagem.getpixel((y + 1, x)))
-                    for pixel in arredores:
-                        if compara(pixel, pixelAtual) > 10 * b:
-                            imagemTransform.putpixel((y, x), (0, 0, 0, 255))
+                        neighbor_pixels.append(get_pixel(imagem, x, y - 1))
+                    if y < height - 1:
+                        neighbor_pixels.append(get_pixel(imagem, x, y + 1))
+                    for pixel in neighbor_pixels:
+                        value_difference = get_hsv_value_difference(
+                            pixel, current_pixel
+                        )
+                        if value_difference > VALUE_THRESHOLD_MULTIPLIER * output_index:
+                            output_image.putpixel((x, y), HIGHLIGHT_COLOR)
                             break
-            imagemTransform.save(nomeSave.format(a, b))
-            imagemTransform.close()
-            print("finalizado " + nomeSave.format(a, b))
+            output_name = f"A{input_index:03d}{output_index:01d}.png"
+            output_image.save(output_name)
+            output_image.close()
+            print(f"{output_name} was created")
         imagem.close()
 
 
