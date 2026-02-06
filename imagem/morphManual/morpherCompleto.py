f838cfe81960525115502f7415faab739c37d3bd
Camilla Alves <arcamilla42@gmail.com>
2026-02-06 16:52:59 -0300

    refactor: uncomment process calls in main function for execution

--- imagem/morphManual/morpherCompleto.py
+++ imagem/morphManual/morpherCompleto.py
@@ -45,10 +45,10 @@ def fazProcesso(processo,nome):
 
 def main() -> None:
     inicioDef = time()
-    #fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\preparaMorph.py ","limpar directory")
-    #fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\analisaEFazConfig.py ","fazer configurações")
-    #fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\recolor.py ","recolor")
-    #fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\morpher.py ","fazer animações")
+    fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\preparaMorph.py ","limpar directory")
+    fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\analisaEFazConfig.py ","fazer configurações")
+    fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\recolor.py ","recolor")
+    fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\morpher.py ","fazer animações")
     fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\corrigeFrames.py ","correção de frames")
     fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\addBackground.py ","fazer backgrounds")
     fazProcesso("python C:\\pythonscript\\imagem\\morphManual\\fazGif.py ","fazer Gif")
