6c39f082ed84768734b09c1d486038efee6568af
Camilla Alves <arcamilla42@gmail.com>
2026-02-06 15:10:05 -0300

    refactor: update nail placement and dimension calculation functions for improved clarity and performance

--- imagem/lineImage/imagemDeLinhas.py
+++ imagem/lineImage/imagemDeLinhas.py
@@ -3,7 +3,20 @@ import os
 import math
 import numpy
 
-BACKGROUND_COLOR = (255, 255, 255)
+BACKGROUND_COLOR = (255, 255, 255, 255)
+NAIL_COLOR = (255, 0, 0, 255)
+LINE_COLOR = (0, 0, 0, 255)
+SAFETY_OFFSET = 1
+FULL_CIRCLE_ANGLE = 360
+HALF_CIRCLE_ANGLE = 180
+NAIL_TOTAL = 400
+MAX_BRIGHTNESS = 255
+COLOR_CHANNELS = 3
+MAX_BRIGHTNESS_TOTAL = MAX_BRIGHTNESS * COLOR_CHANNELS
+ALPHA_CHANNEL = 3
+INPUT_IMAGE = "input.png"
+OUTPUT_IMAGE = "output.png"
+EXAMPLE_IMAGE = "example.png"
 
 CoordData = tuple[int, int]
 ColorData = tuple[int, int, int, int]
@@ -33,88 +46,96 @@ def get_pixel(imagem: Image.Image, coord: tuple[int, int]) -> tuple[int, ...]:
     return pixel
 
 
-def coordenadasPregos(nail_count: int, img: Image.Image) -> list[CoordData]:
-    largura, altura = img.size
-    hip = int(math.sqrt((largura / 2) ** 2 + (altura / 2) ** 2)) + 1
-    print(hip)
-    pregosAngulo = 360 / nail_count
-    pregos: list[CoordData] = []
-    for angulo in numpy.arange(0, 360, pregosAngulo):
-        radiano = angulo * math.pi / 180
-        pregos.append(
-            (int(hip * math.cos(radiano)) + hip, int(hip * math.sin(radiano) + hip))
-        )
-    return pregos
+def place_nails_in_image(image: Image.Image) -> list[CoordData]:
+    width, height = image.size
+    width_squared = (width / 2) ** 2
+    height_squared = (height / 2) ** 2
+    radius_real = math.sqrt(width_squared + height_squared)
+    radius = int(radius_real) + SAFETY_OFFSET
+    print(radius)
+    nail_angle_increment = FULL_CIRCLE_ANGLE / NAIL_TOTAL
+    nail_coordinates: list[CoordData] = []
+    for angle in numpy.arange(0, FULL_CIRCLE_ANGLE, nail_angle_increment):
+        angle_in_radians = angle * math.pi / HALF_CIRCLE_ANGLE
+        angle_cos = math.cos(angle_in_radians) + SAFETY_OFFSET
+        angle_sin = math.sin(angle_in_radians) + SAFETY_OFFSET
+        nail_x = int(radius * angle_cos)
+        nail_y = int(radius * angle_sin)
+        nail_coordinate = (nail_x, nail_y)
+        nail_coordinates.append(nail_coordinate)
+    return nail_coordinates
 
 
-def dimensoesPorPregos(pregos: list[CoordData]) -> tuple[int, int]:
-    maiorX = 0
-    maiorY = 0
-    for prego in pregos:
-        if prego[0] >= maiorX:
-            maiorX = prego[0]
-        if prego[1] >= maiorY:
-            maiorY = prego[1]
-    tamanho = (maiorX + 2, maiorY + 2)
-    return tamanho
+def dimension_by_nails(nail_coordinates: list[CoordData]) -> tuple[int, int]:
+    high_x = 0
+    high_y = 0
+    for nail in nail_coordinates:
+        if nail[0] >= high_x:
+            high_x = nail[0]
+        if nail[1] >= high_y:
+            high_y = nail[1]
+    size = (high_x + 2 * SAFETY_OFFSET, high_y + 2 * SAFETY_OFFSET)
+    return size
 
 
-def ehMaior(
-    pregoProximo: CoordData,
-    pregoAtual: CoordData,
-    menorValor: float,
-    imagemPregos: Image.Image,
+def is_greater(
+    source_nail: CoordData,
+    target_nail: CoordData,
+    lowest_value: float,
+    nail_image: Image.Image,
 ) -> tuple[float, bool]:
-    menorValorOriginal = menorValor
-    totalValor = 0.0
-    totalLinha = 0
-    if pregoAtual == pregoProximo:
-        return (menorValor, False)
-    auxB = 0.0
-    if pregoAtual[0] != pregoProximo[0]:
-        auxA = (pregoAtual[1] - pregoProximo[1]) / (pregoAtual[0] - pregoProximo[0])
-        auxB = pregoProximo[1] - pregoProximo[0] * auxA
-    else:
-        auxA = 2.0
-    if (auxA <= 1) and (auxA >= -1):
-        if pregoAtual[0] > pregoProximo[0]:
+    lowest_value_original = lowest_value
+    total_value = 0.0
+    total_line = 0
+    if source_nail == target_nail:
+        return (lowest_value, False)
+    line_offset = 0.0
+    slope = 2.0
+    source_x, source_y = source_nail
+    target_x, target_y = target_nail
+    if source_x != target_x:
+        difference_x = source_x - target_x
+        difference_y = source_y - target_y
+        slope = difference_y / difference_x
+        line_offset = target_y - target_x * slope
+    if (slope <= 1) and (slope >= -1):
+        flow = 1
+        if source_x > target_x:
             flow = -1
-        else:
-            flow = 1
-        for x in range(pregoAtual[0], pregoProximo[0], flow):
-            pixel = get_pixel(imagemPregos, (x, int(auxA * x + auxB)))
-            if pixel[3] == 255:
-                valor = 0
-                valor += pixel[0]
-                valor += pixel[1]
-                valor += pixel[2]
-                totalValor += valor / (3 * 255)
-                totalLinha += 1
-        if totalLinha == 0:
-            totalLinha = 1
-        if (totalValor / totalLinha) <= menorValor:
-            menorValor = totalValor / totalLinha
+        for x in range(source_x, target_x, flow):
+            y = int(slope * x + line_offset)
+            pixel = get_pixel(nail_image, (x, y))
+            if pixel[ALPHA_CHANNEL] != MAX_BRIGHTNESS:
+                continue
+            value = sum(pixel[0:COLOR_CHANNELS])
+            total_value += value / MAX_BRIGHTNESS_TOTAL
+            total_line += 1
+        if total_line == 0:
+            total_line = 1
+        if (total_value / total_line) <= lowest_value:
+            lowest_value = total_value / total_line
+        has_changed = lowest_value != lowest_value_original
+        return (lowest_value, has_changed)
+    slope = (source_x - target_x) / (source_y - target_y)
+    line_offset = target_x - target_y * slope
+    if source_y > target_y:
+        flow = -1
     else:
-        auxA = (pregoAtual[0] - pregoProximo[0]) / (pregoAtual[1] - pregoProximo[1])
-        auxB = pregoProximo[0] - pregoProximo[1] * auxA
-        if pregoAtual[1] > pregoProximo[1]:
-            flow = -1
-        else:
-            flow = 1
-        for y in range(pregoAtual[1], pregoProximo[1], flow):
-            pixel = get_pixel(imagemPregos, (int(auxA * y + auxB), y))
-            if pixel[3] == 255:
-                valor = 0
-                valor += pixel[0]
-                valor += pixel[1]
-                valor += pixel[2]
-                totalValor += valor / (3 * 255)
-                totalLinha += 1
-        if totalLinha == 0:
-            totalLinha = 1
-        if (totalValor / totalLinha) <= menorValor:
-            menorValor = totalValor / totalLinha
-    return (menorValor, (menorValor != menorValorOriginal))
+        flow = 1
+    for y in range(source_y, target_y, flow):
+        x = int(slope * y + line_offset)
+        pixel = get_pixel(nail_image, (x, y))
+        if pixel[ALPHA_CHANNEL] != MAX_BRIGHTNESS:
+            continue
+        value = sum(pixel[0:COLOR_CHANNELS])
+        total_value += value / MAX_BRIGHTNESS_TOTAL
+        total_line += 1
+    if total_line == 0:
+        total_line = 1
+    if (total_value / total_line) <= lowest_value:
+        lowest_value = total_value / total_line
+    has_changed = lowest_value != lowest_value_original
+    return (lowest_value, has_changed)
 
 
 def draw_line(
@@ -146,45 +167,42 @@ def draw_line(
 
 
 def main() -> None:
-    quantiaPregos = 400
-    imagemPasta = os.path.join("C:\\", "pythonscript", "lineImage", "imagens")
-    imagens = [os.path.join(imagemPasta, imagem) for imagem in os.listdir(imagemPasta)]
-    imagem = Image.open(imagens[5])
-    pregos = coordenadasPregos(quantiaPregos, imagem)
-    largura, altura = imagem.size
-    larguraFinal, alturaFinal = dimensoesPorPregos(pregos)
-    imagemFinal = Image.new("RGBA", (larguraFinal, alturaFinal), (255, 255, 255, 255))
-    imagemPregos = imagemFinal.copy()
-    offset = ((larguraFinal - largura) // 2, (alturaFinal - altura) // 2)
-    imagemPregos.paste(imagem, offset)
-    pregoAtual = pregos[0]
-    imagemExemplo = imagemPregos.copy()
-    for prego in pregos:
-        imagemExemplo.putpixel(prego, (255, 0, 0, 255))
-    imagemExemplo.save(os.path.join(imagemPasta, "valCroche400Exemplo.png"))
+    image = Image.open(INPUT_IMAGE)
+    nails = place_nails_in_image(image)
+    width, height = image.size
+    output_width, output_height = dimension_by_nails(nails)
+    output_image = Image.new("RGBA", (output_width, output_height), BACKGROUND_COLOR)
+    nails_image = output_image.copy()
+    offset = ((output_width - width) // 2, (output_height - height) // 2)
+    nails_image.paste(image, offset)
+    current_nail = nails[0]
+    example_image = nails_image.copy()
+    for nail_coordinate in nails:
+        example_image.putpixel(nail_coordinate, NAIL_COLOR)
+    example_image.save(EXAMPLE_IMAGE)
     while True:
-        menorPrego = pregos[0]
-        menorValor = 1.0
-        for prego in pregos:
-            menorValor, alterou = ehMaior(prego, pregoAtual, menorValor, imagemPregos)
-            if alterou:
-                menorPrego = prego
-        if menorValor != 1.0:
-            print(
-                "do prego "
-                + str(pregos.index(pregoAtual))
-                + " para o prego "
-                + str(pregos.index(menorPrego))
+        closest_nail = nails[0]
+        closest_distance = 1.0
+        for nail_coordinate in nails:
+            closest_distance, has_changed = is_greater(
+                current_nail, nail_coordinate, closest_distance, nails_image
             )
-            imagemPregos = draw_line(
-                pregoAtual, menorPrego, imagemPregos, (255, 255, 255, 255)
-            )
-            imagemFinal = draw_line(pregoAtual, menorPrego, imagemFinal, (0, 0, 0, 255))
-            pregoAtual = menorPrego
-        else:
+            if has_changed:
+                closest_nail = nail_coordinate
+        if closest_distance == 1.0:
             break
-        save_image(imagemFinal, os.path.join(imagemPasta, "out.png"))
-    save_image(imagemFinal, os.path.join(imagemPasta, "final.png"))
+        print(
+            f"do prego {nails.index(current_nail)} para o prego {nails.index(closest_nail)}"
+        )
+        nails_image = draw_line(
+            current_nail, closest_nail, nails_image, BACKGROUND_COLOR
+        )
+        output_image = draw_line(
+            current_nail, closest_nail, output_image, LINE_COLOR
+        )
+        current_nail = closest_nail
+        save_image(output_image, OUTPUT_IMAGE)
+    save_image(output_image, OUTPUT_IMAGE)
 
 
 if __name__ == "__main__":
