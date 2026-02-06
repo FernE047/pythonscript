9526e488321843b4fa7b60848216a51e2082119c
Camilla Alves <arcamilla42@gmail.com>
2026-02-06 08:54:25 -0300

    refactor: streamline image processing logic and improve variable naming for clarity

--- imagem/grafico Imagem/turnToGraf.py
+++ imagem/grafico Imagem/turnToGraf.py
@@ -1,161 +1,193 @@
 from PIL import Image, ImageDraw
 
+# this script takes an image and flattens it by using radial lines from the center. it's funny to use on Luigi's images, his nose becomes a big blob. poor mario brother.
+
+IMAGE_NUMBER = "6"
+SOURCE_IMAGE_PATH = f"images_{IMAGE_NUMBER}.jpg"
+OUTPUT_IMAGE_PATH = f"images_graf_{IMAGE_NUMBER}.jpg"
+EVEN_SAFETY_OFFSET = 2
+ODD_SAFETY_OFFSET = 3
+SCALING_FACTOR = 2
+SCALING_SAFETY_OFFSET = 4
+BACKGROUND_COLOR = (255, 255, 255, 255)
+LINE_COLOR = (0, 0, 0, 255)
+PRINTING_THRESHOLD = 50
+LINE_WIDTH = 1
+
+
+def create_base_image() -> Image.Image:
+    image_source = Image.open(SOURCE_IMAGE_PATH)
+    size = image_source.size
+    width, height = size
+    width += EVEN_SAFETY_OFFSET if width % 2 == 0 else ODD_SAFETY_OFFSET
+    height += EVEN_SAFETY_OFFSET if height % 2 == 0 else ODD_SAFETY_OFFSET
+    base_image = Image.new("RGBA", (width, height))
+    base_image.paste(image_source, (1, 1))
+    print("the base image was created")
+    return base_image
+
 
 def main() -> None:
-    imagemNumber = "6"
-
-    imagemOrigem = Image.open("images" + imagemNumber + ".jpg")
-    tamanho = imagemOrigem.size
-    width, height = tamanho
-    if (width % 2 == 0) and (height % 2 == 0):
-        imagemBest = Image.new("RGBA", (width + 3, height + 3))
-        imagemBest.paste(imagemOrigem, (1, 1))
-    elif width % 2 == 0:
-        imagemBest = Image.new("RGBA", (width + 3, height + 2))
-        imagemBest.paste(imagemOrigem, (1, 1))
-    elif height % 2 == 0:
-        imagemBest = Image.new("RGBA", (width + 2, height + 3))
-        imagemBest.paste(imagemOrigem, (1, 1))
-    else:
-        imagemBest = Image.new("RGBA", (width + 2, height + 2))
-        imagemBest.paste(imagemOrigem, (1, 1))
-    print("melhor imagem feita")
-    tamanho = imagemBest.size
-    width, height = tamanho
-    meio = (int((width + 1) / 2), int((height + 1) / 2))
-    extensao = 2 * width + 2 * height + 4
-    imageGraf = Image.new("RGB", (extensao, height))
-    print(str(extensao))
-    a = 0
-    for aheight in range(0, meio[1] + 1):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], 0, aheight), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if aheight % 50 == 0:
-            print(str(aheight))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(0, meio[1] + 1):
-            for x in range(0, meio[0] + 1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    for aheight in range(meio[1] + 1, height):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], 0, aheight), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if aheight % 50 == 0:
-            print(str(aheight))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(height - 1, meio[1] - 1, -1):
-            for x in range(0, meio[0] + 1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    for awidth in range(0, meio[0] + 1):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], awidth, height - 1), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if awidth % 50 == 0:
-            print(str(awidth))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(height - 1, meio[1] - 1, -1):
-            for x in range(0, meio[0] + 1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    for awidth in range(meio[0] + 1, width):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], awidth, height - 1), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if awidth % 50 == 0:
-            print(str(awidth))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(height - 1, meio[1] - 1, -1):
-            for x in range(width - 1, meio[0] - 1, -1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    for aheight in range(height - 1, meio[1] - 1, -1):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], width - 1, aheight), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if aheight % 50 == 0:
-            print(str(aheight))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(height - 1, meio[1] - 1, -1):
-            for x in range(width - 1, meio[0] - 1, -1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    for aheight in range(meio[1], -1, -1):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], width - 1, aheight), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if aheight % 50 == 0:
-            print(str(aheight))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(0, meio[1] + 1):
-            for x in range(width - 1, meio[0] - 1, -1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    for awidth in range(width - 1, meio[0] - 1, -1):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], awidth, 0), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if awidth % 50 == 0:
-            print(str(awidth))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(0, meio[1] + 1):
-            for x in range(width - 1, meio[0] - 1, -1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    for awidth in range(meio[0], -1, -1):
-        lineImage = Image.new("RGBA", (width, height), (255, 255, 255, 255))
-        draw = ImageDraw.Draw(lineImage)
-        draw.line(
-            (meio[0], meio[1], awidth, 0), fill=(0, 0, 0, 255), width=1
-        )  # (firstX,firstY,secondX,secondY)
-        altGraf = 0
-        if awidth % 50 == 0:
-            print(str(awidth))
-            imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
-        for y in range(0, meio[1] + 1):
-            for x in range(0, meio[0] + 1):
-                if lineImage.getpixel((x, y)) == (0, 0, 0, 255):
-                    imageGraf.putpixel((a, altGraf), imagemBest.getpixel((x, y)))
-                    altGraf += 1
-        a += 1
-    imageGraf.save("imagesgraf" + imagemNumber + ".jpg")
+    base_image = create_base_image()
+    width, height = base_image.size
+    mid_point = (int((width + 1) / 2), int((height + 1) / 2))
+    graph_width = int(SCALING_FACTOR * (width + height) + SCALING_SAFETY_OFFSET)
+    flat_image = Image.new("RGB", (graph_width, height))
+    print(graph_width)
+    graf_x = 0
+    for line_y in range(0, mid_point[1] + 1):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], 0, line_y)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_y % PRINTING_THRESHOLD == 0:
+            print(line_y)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(0, mid_point[1] + 1):
+            for x in range(0, mid_point[0] + 1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    for line_y in range(mid_point[1] + 1, height):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], 0, line_y)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_y % PRINTING_THRESHOLD == 0:
+            print(line_y)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(height - 1, mid_point[1] - 1, -1):
+            for x in range(0, mid_point[0] + 1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    for line_x in range(0, mid_point[0] + 1):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], line_x, height - 1)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_x % PRINTING_THRESHOLD == 0:
+            print(line_x)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(height - 1, mid_point[1] - 1, -1):
+            for x in range(0, mid_point[0] + 1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    for line_x in range(mid_point[0] + 1, width):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], line_x, height - 1)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_x % PRINTING_THRESHOLD == 0:
+            print(line_x)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(height - 1, mid_point[1] - 1, -1):
+            for x in range(width - 1, mid_point[0] - 1, -1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    for line_y in range(height - 1, mid_point[1] - 1, -1):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], width - 1, line_y)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_y % PRINTING_THRESHOLD == 0:
+            print(line_y)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(height - 1, mid_point[1] - 1, -1):
+            for x in range(width - 1, mid_point[0] - 1, -1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    for line_y in range(mid_point[1], -1, -1):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], width - 1, line_y)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_y % PRINTING_THRESHOLD == 0:
+            print(line_y)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(0, mid_point[1] + 1):
+            for x in range(width - 1, mid_point[0] - 1, -1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    for line_x in range(width - 1, mid_point[0] - 1, -1):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], line_x, 0)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_x % PRINTING_THRESHOLD == 0:
+            print(line_x)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(0, mid_point[1] + 1):
+            for x in range(width - 1, mid_point[0] - 1, -1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    for line_x in range(mid_point[0], -1, -1):
+        line_image = Image.new("RGBA", (width, height), BACKGROUND_COLOR)
+        draw = ImageDraw.Draw(line_image)
+        line_coords = (mid_point[0], mid_point[1], line_x, 0)
+        draw.line(line_coords, fill=LINE_COLOR, width=LINE_WIDTH)
+        graf_y = 0
+        if line_x % PRINTING_THRESHOLD == 0:
+            print(line_x)
+            flat_image.save(OUTPUT_IMAGE_PATH)
+        for y in range(0, mid_point[1] + 1):
+            for x in range(0, mid_point[0] + 1):
+                if line_image.getpixel((x, y)) != LINE_COLOR:
+                    continue
+                pixel = base_image.getpixel((x, y))
+                if pixel is None:
+                    pixel = BACKGROUND_COLOR
+                flat_image.putpixel((graf_x, graf_y), pixel)
+                graf_y += 1
+        graf_x += 1
+    flat_image.save(OUTPUT_IMAGE_PATH)
 
 
 if __name__ == "__main__":
