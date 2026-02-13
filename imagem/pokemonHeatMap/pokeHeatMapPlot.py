35f79e367aea57b93b40138414102d147e848509
Camilla Alves <arcamilla42@gmail.com>
2026-02-13 15:42:52 -0300

    refactor: enhance heat map plotting by consolidating color handling and improving readability

--- imagem/pokemonHeatMap/pokeHeatMapPlot.py
+++ imagem/pokemonHeatMap/pokeHeatMapPlot.py
@@ -1,41 +1,142 @@
+#type: ignore
+
+#plt doesn't have good type hints for strict mode, so we ignore all type errors in this file
+
 import matplotlib.pyplot as plt
-import matplotlib.tri as tri
 import numpy as np
-import math
 import shelve
-import os
+
+MAX_TICKS = 7
+TICK_SIZE = 127
+TICK_ARRAY = np.sort(np.array([TICK_SIZE * i for i in range(MAX_TICKS + 1)]))
+ALL_PLOT_COLORS = (
+    "viridis",
+    "plasma",
+    "inferno",
+    "magma",
+    "Greys",
+    "Purples",
+    "Blues",
+    "Greens",
+    "Oranges",
+    "Reds",
+    "YlOrBr",
+    "YlOrRd",
+    "OrRd",
+    "PuRd",
+    "RdPu",
+    "BuPu",
+    "GnBu",
+    "PuBu",
+    "PuBuGn",
+    "BuGn",
+    "YlGn",
+    "binary",
+    "gist_yarg",
+    "gist_gray",
+    "gray",
+    "bone",
+    "pink",
+    "spring",
+    "summer",
+    "autumn",
+    "winter",
+    "cool",
+    "Wistia",
+    "hot",
+    "afmhot",
+    "gist_heat",
+    "copper",
+    "PiYG",
+    "PRGn",
+    "BrBG",
+    "PuOr",
+    "RdGy",
+    "RdBu",
+    "RdYlBu",
+    "RdYlGn",
+    "Spectral",
+    "coolwarm",
+    "bwr",
+    "seismic",
+    "Pastel1",
+    "Pastel2",
+    "Paired",
+    "Accent",
+    "Dark2",
+    "Set1",
+    "Set2",
+    "Set3",
+    "tab10",
+    "tab20",
+    "tab20b",
+    "tab20c",
+    "flag",
+    "prism",
+    "ocean",
+    "gist_earth",
+    "terrain",
+    "gist_stern",
+    "gnuplot",
+    "gnuplot2",
+    "CMRmap",
+    "cubehelix",
+    "brg",
+    "hsv",
+    "gist_rainbow",
+    "rainbow",
+    "jet",
+    "nipy_spectral",
+    "gist_ncar",
+)
 
 
 def main() -> None:
-    with shelve.open(os.path.join(os.getcwd(), "dadosPreProcessados")) as database:
-        xHeat=database["x"]
-        yHeat=database["y"]
-        zHeat=database["z"]
+    with shelve.open("./dadosPreProcessados") as database:
+        xHeat = database["x"]
+        yHeat = database["y"]
+        zHeat = database["z"]
 
-    """plt.figure()
+    plt.figure()
     ax = plt.gca()
     ax.set_aspect("equal")
-    color="hsv"
-    cmap=plt.get_cmap(color)
-    CS = ax.tricontourf(xHeat, yHeat, zHeat, np.linspace(0,762,256),cmap=cmap)
-    cbar = plt.colorbar(CS, ticks=np.sort(np.array([0,127,254,381,508,635,762])),ax=ax, orientation="horizontal", shrink=.75, pad=.09, aspect=40,fraction=0.05)
-    cbar.ax.set_xticklabels(list(map(str,np.sort(np.array([0,127,254,381,508,635,762])))))  # horizontal colorbar
-    cbar.ax.tick_params(labelsize=8) 
+    color = "hsv"
+    cmap = plt.get_cmap(color)
+    cs = ax.tricontourf(xHeat, yHeat, zHeat, np.linspace(0, 762, 256), cmap=cmap)
+    cbar = plt.colorbar(
+        cs,
+        ticks=TICK_ARRAY,
+        ax=ax,
+        orientation="horizontal",
+        shrink=0.75,
+        pad=0.09,
+        aspect=40,
+        fraction=0.05,
+    )
+    cbar.ax.set_xticklabels(list(map(str, TICK_ARRAY)))  # horizontal colorbar
+    cbar.ax.tick_params(labelsize=8)
     plt.title(f"Heat Map {color}")
     plt.xlabel("X Label")
     plt.ylabel("Y Label")
-    plt.show()"""
-    #"viridis","plasma","inferno","magma","Greys","Purples","Blues","Greens","Oranges","Reds","YlOrBr","YlOrRd","OrRd","PuRd","RdPu","BuPu","GnBu","PuBu","PuBuGn","BuGn","YlGn","binary","gist_yarg","gist_gray","gray","bone","pink","spring","summer","autumn","winter","cool","Wistia","hot","afmhot","gist_heat","copper",
-    colors=["PiYG","PRGn","BrBG","PuOr","RdGy","RdBu","RdYlBu","RdYlGn","Spectral","coolwarm","bwr","seismic","Pastel1","Pastel2","Paired","Accent","Dark2","Set1","Set2","Set3","tab10","tab20","tab20b","tab20c","flag","prism","ocean","gist_earth","terrain","gist_stern","gnuplot","gnuplot2","CMRmap","cubehelix","brg","hsv","gist_rainbow","rainbow","jet","nipy_spectral","gist_ncar"]
-    for color in colors:
+    plt.show()
+    for color in ALL_PLOT_COLORS:
         plt.figure()
         ax = plt.gca()
         ax.set_aspect("equal")
-        cmap=plt.get_cmap(color)
-        CS = ax.tricontourf(xHeat, yHeat, zHeat, np.linspace(0,762,256),cmap=cmap)
-        cbar = plt.colorbar(CS, ticks=np.sort(np.array([0,127,254,381,508,635,762])),ax=ax, orientation="horizontal", shrink=.75, pad=.09, aspect=40,fraction=0.05)
-        cbar.ax.set_xticklabels(list(map(str,np.sort(np.array([0,127,254,381,508,635,762])))))  # horizontal colorbar
-        cbar.ax.tick_params(labelsize=8) 
+        cmap = plt.get_cmap(color)
+        cs = ax.tricontourf(xHeat, yHeat, zHeat, np.linspace(0, 762, 256), cmap=cmap)
+        cbar = plt.colorbar(
+            cs,
+            ticks=TICK_ARRAY,
+            ax=ax,
+            orientation="horizontal",
+            shrink=0.75,
+            pad=0.09,
+            aspect=40,
+            fraction=0.05,
+        )
+        cbar.ax.set_xticklabels(list(map(str, TICK_ARRAY)))  # horizontal colorbar
+        cbar.ax.tick_params(labelsize=8)
         plt.title(f"Heat Map {color}")
         plt.xlabel("X Label")
         plt.ylabel("Y Label")
