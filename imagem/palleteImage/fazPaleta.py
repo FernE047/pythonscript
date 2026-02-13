5909f0539fe277567ead3eb3e94fe9c7cb718a52
Camilla Alves <arcamilla42@gmail.com>
2026-02-13 10:10:46 -0300

    refactor: streamline image generation logic and enhance readability in palleteImage script

--- imagem/palleteImage/fazPaleta.py
+++ imagem/palleteImage/fazPaleta.py
@@ -1,16 +1,24 @@
+import math
 from PIL import Image
 import os
 
+PALLETE_FOLDER = "paleta"
+if not os.path.exists(PALLETE_FOLDER):
+    os.makedirs(PALLETE_FOLDER)
+PALLETE_IMAGE = os.path.join(PALLETE_FOLDER, "paletaBW4.png")
+MAX_BRIGHTNESS = 256
+MAX_SIZE = math.ceil(math.sqrt(MAX_BRIGHTNESS))
+TRANSPARENT = (0, 0, 0, 0)
 
 def main() -> None:
-    imagem=Image.new("RGBA",(16,16),(0,0,0,0))
-    imagem.save(os.path.join("paleta","paletaBW4.png"))
-    for x in range(256):
-        y=int(x/16)
-        cor=tuple([x for a in range(3)]+[255])
-        print(cor)
-        imagem.putpixel((x%16,y),cor)
-    imagem.save(os.path.join("paleta","paletaBW4.png"))
+    image = Image.new("RGBA", (MAX_SIZE, MAX_SIZE), TRANSPARENT)
+    image.save(PALLETE_IMAGE)
+    for x in range(MAX_BRIGHTNESS):
+        y = int(x / MAX_SIZE)
+        color_value = tuple([x for _ in range(3)] + [MAX_BRIGHTNESS])
+        print(color_value)
+        image.putpixel((x % MAX_SIZE, y), color_value)
+    image.save(PALLETE_IMAGE)
 
 
 if __name__ == "__main__":
