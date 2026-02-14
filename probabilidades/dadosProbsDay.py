5854053d12f7ca8b973dfd20aa9e7a8f3d7aecf7
Camilla Reis <camillebay25@gmail.com>
2026-02-14 14:40:26 -0300

    refactor: improve code readability and formatting in dadosProbsDay.py and webscraptest.py

--- probabilidades/dadosProbsDay.py
+++ probabilidades/dadosProbsDay.py
@@ -3,37 +3,37 @@
 
 
 def main() -> None:
-	for a in range(0,20):
-		print(f"\nquantia de dados: {a+1}")
-		vezes=6**a
-		minuto=0
-		hora=0
-		dia=0
-		ano=0
-		seculo=0
-		if(vezes>60):
-			minuto=int(vezes/12)
-		else:
-			print(f"segundos: {vezes}")
-		if(minuto>=1):
-			hora=int(minuto/60)
-			minuto=minuto%60
-			print(f"minuto: {minuto}")
-		if(hora>=1):
-			dia=int(hora/24)
-			hora=hora%24
-			print(f"horas: {hora}")
-		if(dia>=1):
-			ano=int(dia/365.25)
-			dia=dia%365
-			print(f"dia: {dia}")
-		if(ano>=1):
-			seculo=int(ano/100)
-			ano=ano%100
-			print(f"ano: {ano}")
-		if(seculo>=1):
-			print(f"seculo: {seculo}")
-		print(f"probabilidade: 1 em {vezes}")
+    for a in range(0, 20):
+        print(f"\nquantia de dados: {a + 1}")
+        vezes = 6**a
+        minuto = 0
+        hora = 0
+        dia = 0
+        ano = 0
+        seculo = 0
+        if vezes > 60:
+            minuto = int(vezes / 12)
+        else:
+            print(f"segundos: {vezes}")
+        if minuto >= 1:
+            hora = int(minuto / 60)
+            minuto = minuto % 60
+            print(f"minuto: {minuto}")
+        if hora >= 1:
+            dia = int(hora / 24)
+            hora = hora % 24
+            print(f"horas: {hora}")
+        if dia >= 1:
+            ano = int(dia / 365.25)
+            dia = dia % 365
+            print(f"dia: {dia}")
+        if ano >= 1:
+            seculo = int(ano / 100)
+            ano = ano % 100
+            print(f"ano: {ano}")
+        if seculo >= 1:
+            print(f"seculo: {seculo}")
+        print(f"probabilidade: 1 em {vezes}")
 
 
 if __name__ == "__main__":
--- web/google/webscraptest.py
+++ web/google/webscraptest.py
@@ -3,62 +3,127 @@ import requests
 import bs4
 import re
 
-def resultadosQuantia(termo):
-    pesquisa=requests.get(f"https://www.google.com.br/search?q={termo}")
-    while(pesquisa.status_code != requests.codes.ok):
-        pesquisa=requests.get(f"https://www.google.com.br/search?q={termo}") 
-    googleSoup = bs4.BeautifulSoup(pesquisa.text,features="html.parser")        #arruma o HTML
-    informacao=googleSoup.select("#resultStats")                                #procura a ID resultstats onde fica a informação
-    pegaNumero=re.compile(r"\d{1,3}")                                           #cria um regex para retirada dos números dos pontos
-    textoMisturado=informacao[0].getText()                                      #cria um texto "aproximadamente x resultados"
-    if(textoMisturado):
-        numeroTexto=pegaNumero.findall(textoMisturado)                          #aplica regex para captura do numero
-        numero=int("".join(numeroTexto))
-    else:
-        numero=0
-    print(numero)
-    return(numero)
 
-def encontrarzero(termo,level=0,levelAtual=0):
-    possibilidade=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+def resultadosQuantia(termo):
+    pesquisa = requests.get(f"https://www.google.com.br/search?q={termo}")
+    while pesquisa.status_code != requests.codes.ok:
+        pesquisa = requests.get(f"https://www.google.com.br/search?q={termo}")
+    googleSoup = bs4.BeautifulSoup(
+        pesquisa.text, features="html.parser"
+    )  # arruma o HTML
+    informacao = googleSoup.select(
+        "#resultStats"
+    )  # procura a ID resultstats onde fica a informação
+    pegaNumero = re.compile(
+        r"\d{1,3}"
+    )  # cria um regex para retirada dos números dos pontos
+    textoMisturado = informacao[
+        0
+    ].getText()  # cria um texto "aproximadamente x resultados"
+    if textoMisturado:
+        numeroTexto = pegaNumero.findall(
+            textoMisturado
+        )  # aplica regex para captura do numero
+        numero = int("".join(numeroTexto))
+    else:
+        numero = 0
+    print(numero)
+    return numero
+
+
+def encontrarzero(termo, level=0, levelAtual=0):
+    possibilidade = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
     for a in possibilidade:
-        termo=list(termo)
+        termo = list(termo)
         termo.append(a)
-        termo=str("".join(termo))
+        termo = str("".join(termo))
         print(termo)
-        if (levelAtual<level):
-            levelAtual+=1
-            b=encontrarzero(termo,level,levelAtual)
-            if(b!=0):
-                return(termo)
+        if levelAtual < level:
+            levelAtual += 1
+            b = encontrarzero(termo, level, levelAtual)
+            if b != 0:
+                return termo
             else:
-                termo=list(termo)
-                del termo[(len(termo)-1)]
-                termo=("".join(termo))
-        elif(int(resultadosQuantia(termo))==0):
-            return(termo)
-        termo=list(termo)
-        del termo[(len(termo)-1)]
-        termo=("".join(termo))
-    return(0)			
+                termo = list(termo)
+                del termo[(len(termo) - 1)]
+                termo = "".join(termo)
+        elif int(resultadosQuantia(termo)) == 0:
+            return termo
+        termo = list(termo)
+        del termo[(len(termo) - 1)]
+        termo = "".join(termo)
+    return 0
+
 
 def encontrar(termo):
-    level=0
-    levelAtual=0
-    termoFinal=encontrarzero(termo,level,0)
-    while(termoFinal==0):
-        level+=1
-        termoFinal=encontrarzero(termo,level,0)
-    return(termoFinal)
-
+    level = 0
+    levelAtual = 0
+    termoFinal = encontrarzero(termo, level, 0)
+    while termoFinal == 0:
+        level += 1
+        termoFinal = encontrarzero(termo, level, 0)
+    return termoFinal
 
 
 def main() -> None:
     print("digite o termo de pesquisa")
-    termoInicial=input()
-    #print("digite o modo de operação:\n1:primeiro\n2:porcentagem\n3:menor numero")
-    #modo=int(input())
-    resultados=encontrar(termoInicial)
+    termoInicial = input()
+    # print("digite o modo de operação:\n1:primeiro\n2:porcentagem\n3:menor numero")
+    # modo=int(input())
+    resultados = encontrar(termoInicial)
     print(f"\n\nresultado:\n{resultados}")
 
 
