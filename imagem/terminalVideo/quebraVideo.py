f0c03adf3934a04b3ac0f66c5315bef2f1215a62
Camilla Reis <camillebay25@gmail.com>
2026-02-15 20:03:26 -0300

    refactor: use constant for image extension in video frame extraction logic

--- imagem/terminalVideo/quebraVideo.py
+++ imagem/terminalVideo/quebraVideo.py
@@ -4,16 +4,17 @@ import shutil
 FRAMES_FOLDER = "./video"  # complete
 VIDEO_NAME = "example"
 OUTPUT_FOLDER = "./video2"
+EXTENSION_USED = ".jpg"
 
 
 def main() -> None:
-    os.system(f"ffmpeg -i {VIDEO_NAME}.mp4 thumb_%06d.jpg -hide_banner")
+    os.system(f"ffmpeg -i {VIDEO_NAME}.mp4 thumb_%06d{EXTENSION_USED} -hide_banner")
     print("Done, moving files...")
     frames_raw = os.listdir(FRAMES_FOLDER)
     frames = [f"{FRAMES_FOLDER}/{frame}" for frame in frames_raw]
     for frame in frames:
         _, extension = os.path.splitext(frame)
-        if extension != ".jpg":
+        if extension != EXTENSION_USED:
             continue
         shutil.move(frame, OUTPUT_FOLDER)
 
