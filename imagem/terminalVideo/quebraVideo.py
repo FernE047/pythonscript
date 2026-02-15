811880032e458af41085c60f54916bf165fabc96
Camilla Reis <camillebay25@gmail.com>
2026-02-15 20:04:28 -0300

    refactor: update video name handling and streamline ffmpeg command in quebraVideo.py

--- imagem/terminalVideo/quebraVideo.py
+++ imagem/terminalVideo/quebraVideo.py
@@ -2,13 +2,15 @@ import os
 import shutil
 
 FRAMES_FOLDER = "./video"  # complete
-VIDEO_NAME = "example"
+VIDEO_NAME = "example.mp4"
 OUTPUT_FOLDER = "./video2"
 EXTENSION_USED = ".jpg"
+FFMPEG_PATH = "ffmpeg"
+FRAMES_TEMPLATE = f"thumb_%06d{EXTENSION_USED}"
 
 
 def main() -> None:
-    os.system(f"ffmpeg -i {VIDEO_NAME}.mp4 thumb_%06d{EXTENSION_USED} -hide_banner")
+    os.system(f"{FFMPEG_PATH} -i {VIDEO_NAME} {FRAMES_TEMPLATE} -hide_banner")
     print("Done, moving files...")
     frames_raw = os.listdir(FRAMES_FOLDER)
     frames = [f"{FRAMES_FOLDER}/{frame}" for frame in frames_raw]
