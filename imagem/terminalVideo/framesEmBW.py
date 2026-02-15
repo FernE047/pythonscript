493abdcb5f0347a508a3aaf7cb0d5812eebdc6cc
Camilla Reis <camillebay25@gmail.com>
2026-02-15 19:59:37 -0300

    refactor: streamline image processing and pixel retrieval in framesEmBW.py

--- imagem/terminalVideo/framesEmBW.py
+++ imagem/terminalVideo/framesEmBW.py
@@ -1,6 +1,22 @@
 from PIL import Image
 import os
 
+LEVEL_COUNT = 8
+MAX_BRIGHTNESS = 256
+BRIGHTNESS_STEP = MAX_BRIGHTNESS // LEVEL_COUNT
+FRAMES_FOLDER = "./video"
+
+CoordData = tuple[int, int]
+
+
+def get_pixel(image: Image.Image, coord: CoordData) -> float | int:
+    pixel = image.getpixel(coord)
+    if pixel is None:
+        raise ValueError("Pixel not found")
+    if isinstance(pixel, tuple):
+        raise ValueError("Image is not in grayscale mode")
+    return pixel
+
 
 def open_image(image_path: str) -> Image.Image:
     with Image.open(image_path) as image:
@@ -8,36 +24,34 @@ def open_image(image_path: str) -> Image.Image:
         return image_in_memory
 
 
-def imagemToText(imagem):
-    global showAltura
-    global niveis
-    global showLargura
-    imagemColor = open_image(imagem)
-    imagemBW = imagemColor.convert("L")
-    largura, altura = imagemBW.size
-    if showAltura == 0:
-        if largura > showLargura:
-            aspectRatio = showLargura / largura
-            showAltura = int(altura * aspectRatio)
+def frame_to_text(imagem: str) -> None:
+    display_height = 0
+    display_width = 80
+    color_image = open_image(imagem)
+    grayscale_image = color_image.convert("L")
+    width, height = grayscale_image.size
+    if display_height == 0:
+        if width > display_width:
+            aspectRatio = display_width / width
+            display_height = int(height * aspectRatio)
         else:
-            showAltura = altura
-    imagemShow = imagemBW.resize((showLargura, showAltura))
-    largura, altura = imagemShow.size
-    for y in range(altura):
-        for x in range(largura):
-            pixel = imagemShow.getpixel((x, y))
-            imagemShow.putpixel((x, y), int(pixel / 32) * 32 - 1)
-    imagemShow.save(imagem)
+            display_height = height
+    display_image = grayscale_image.resize((display_width, display_height))
+    width, height = display_image.size
+    for y in range(height):
+        for x in range(width):
+            pixel = get_pixel(display_image, (x, y))
+            step = pixel // BRIGHTNESS_STEP
+            grayscale_value = step * BRIGHTNESS_STEP - 1
+            display_image.putpixel((x, y), grayscale_value)
+    display_image.save(imagem)
 
 
 def main() -> None:
-    showAltura = 0
-    niveis = [" ", "`", ".", ",", "+", "%", "@", "#"]
-    showLargura = 80
-    diretorio = "./video"
-    imagens = [os.path.join(diretorio, imagem) for imagem in os.listdir(diretorio)]
-    for imagem in imagens:
-        imagemToText(imagem)
+    frames_raw = os.listdir(FRAMES_FOLDER)
+    frames = [f"{FRAMES_FOLDER}/{frame}" for frame in frames_raw]
+    for frame in frames:
+        frame_to_text(frame)
 
 
 if __name__ == "__main__":
