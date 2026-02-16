4cbf28a91fd4d783de147942340732c55e304ba9
Camilla Alves <arcamilla42@gmail.com>
2026-02-16 11:32:06 -0300

    refactor: improve code structure and readability in perfectMario script

--- jogos/perfectGameplay/perfectMario.py
+++ jogos/perfectGameplay/perfectMario.py
@@ -1,28 +1,40 @@
-#! python3
-# superHotEverything.py - Displays the mouse cursor"s current position.
 import pyautogui
 import time
 
+# this script was a silly attempt to make a perfect gameplay for mario, using an emulator. it doesn't work anymore because you need to have specific screen resolution and the game has to be in a specific position on the screen, but it was fun to make and it can be used as a base for other similar projects.
 
-def pressFor(tecla, tempo=1):
-    print(f"press {tecla}")
-    pyautogui.keyDown(tecla)
-    time.sleep(tempo)
-    pyautogui.keyUp(tecla)
+TIME_DEFAULT = 1.0
+WINDOWS_MENU = (340, 750)
+EMULATOR_POSITION = (340, 500)
+MARIO_LEGAL_OBTAINED_ROM = "Super_mario_brothers.nst"
+EXTRA_MENU_CLOSE = (900, 600)
+
+
+def pressFor(key_to_press: str, time_to_press: float = TIME_DEFAULT) -> None:
+    print(f"press {key_to_press}")
+    pyautogui.keyDown(key_to_press)
+    time.sleep(time_to_press)
+    pyautogui.keyUp(key_to_press)
+
+
+def click(coord: tuple[int, int]) -> None:
+    x, y = coord
+    print(f"clicking at {x}, {y}")
+    pyautogui.click(x, y)
 
 
 def main() -> None:
     try:
-        pyautogui.click(340, 750)
-        pyautogui.click(340, 500)
+        click(WINDOWS_MENU)
+        click(EMULATOR_POSITION)
         time.sleep(1)
         pressFor("f7")
-        pyautogui.typewrite("Super_mario_brothers.nst")
+        pyautogui.typewrite(MARIO_LEGAL_OBTAINED_ROM)
         pressFor("enter")
         time.sleep(2)
         pressFor("enter", 1)
         pressFor("l", 10)
-        pyautogui.click(900, 600)
+        click(EXTRA_MENU_CLOSE)
     except KeyboardInterrupt:
         print("\nDone.")
 
