84ddd5997fac5b17948c39462ae79ccd442a695e
Camilla Reis <camillebay25@gmail.com>
2026-02-15 22:32:11 -0300

    refactor: improve code readability and structure in coin toss game

--- jogos/coinTosses.py
+++ jogos/coinTosses.py
@@ -1,5 +1,6 @@
-
 import random
+
+# script done while taking a crash course in python, it is a simple coin toss game where the user has to guess the outcome of a coin toss (heads or tails) and gets two chances to guess correctly.
 
 
 def main() -> None:
@@ -7,16 +8,16 @@ def main() -> None:
     while guess not in ("heads", "tails"):
         print("Guess the coin toss! Enter heads or tails:")
         guess = input()
-    toss = ["tails","heads"][random.randint(0, 1)] # 0 is tails, 1 is heads
+    toss = ["tails", "heads"][random.randint(0, 1)]  # 0 is tails, 1 is heads
+    if toss == guess:
+        print("You got it!")
+        return
+    print("Nope! Guess again!")
+    guess = input()
     if toss == guess:
         print("You got it!")
     else:
-        print("Nope! Guess again!")
-        guesss = input()
-        if toss == guess:
-            print("You got it!")
-        else:
-            print("Nope. You are really bad at this game.")
+        print("Nope. You are really bad at this game.")
 
 
 if __name__ == "__main__":
