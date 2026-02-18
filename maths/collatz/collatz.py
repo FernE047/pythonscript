c6c7d97023744cc74625a041e957a7b752547953
Camilla Alves <arcamilla42@gmail.com>
2026-02-18 17:00:47 -0300

    refactor: improve collatz function logic and standardize user prompts

--- maths/collatz/collatz.py
+++ maths/collatz/collatz.py
@@ -1,29 +1,26 @@
-def collatz(numero):
-    print(str(numero))
-    if (numero != 1) and (numero % 2):
-        numero = 3 * numero + 1
-    elif numero % 2 == 0:
-        numero = int(numero / 2)
+def collatz(number_input: int) -> int:
+    print(str(number_input))
+    if (number_input != 1) and (number_input % 2):
+        return collatz(3 * number_input + 1)
+    elif number_input % 2 == 0:
+        return collatz(int(number_input / 2))
     else:
-        return
-    collatz(numero)
-
+        return 1
 
 
 def main() -> None:
     while True:
-        print("digite um numero:")
+        print("enter a number:")
         try:
             valor = int(input())
-            if valor <= 0:
-                print("somente numeros positivos")
-                continue
-            else:
-                collatz(valor)
-                break
         except ValueError:
-            print("Somente numeros são permitidos")
+            print("Only numbers are allowed")
             continue
+        if valor <= 0:
+            print("Only Positive numbers are allowed")
+            continue
+        collatz(valor)
+        break
 
 
 if __name__ == "__main__":
