4365c7320b8a3bec6c1679f4c93f2e8371986901
Camilla Alves <arcamilla42@gmail.com>
2026-02-19 16:17:47 -0300

    refactor: clean up collatz functions and improve plotting logic

--- maths/collatzSecondTake/multiCollatzPlot.py
+++ maths/collatzSecondTake/multiCollatzPlot.py
@@ -1,87 +1,82 @@
 from matplotlib import pyplot as plt
 
 
-def mod(lista, x):  # base,resto,x):
-    base, resto = lista
-    if x % base == resto:
-        return True
+COLLATZ_RULES = (
+    ((2, 0), (1, 0, 2)),
+    ((8, 5), (1, -1, 4)),
+    ((8, 7), (3, 1, 2)),
+    ((16, 3), (1, -1, 2)),
+    ((16, 9), (3, 1, 4)),
+    ((16, 11), (3, 1, 2)),
+    ((32, 1), (3, 1, 4)),
+    ((64, 17), (3, -3, 16)),
+    ((128, 49), (3, -3, 16)),
+    ((128, 113), (1, -17, 32)),
+)
+
+def mod(data: tuple[int, int], x: int) -> bool:  # base,resto,x):
+    base, remainder = data
+    return x % base == remainder
 
 
-def formula(lista, x):  # a,b,c,x):
-    a, b, c = lista
-    return (a * x + b) / c
+def formula(data: tuple[int, int, int], x: int) -> int:  # a,b,c,x:
+    a, b, c = data
+    return (a * x + b) // c
 
 
-def collatz(x):
+def collatz(x: int) -> int:
     if x % 2:
         return 3 * x + 1
     else:
-        return x / 2
+        return x // 2
 
 
-def wazoo(x):
-    regras = (
-        ((2, 0), (1, 0, 2)),
-        ((8, 5), (1, -1, 4)),
-        ((8, 7), (3, 1, 2)),
-        ((16, 3), (1, -1, 2)),
-        ((16, 9), (3, 1, 4)),
-        ((16, 11), (3, 1, 2)),
-        ((32, 1), (3, 1, 4)),
-        ((64, 17), (3, -3, 16)),
-        ((128, 49), (3, -3, 16)),
-        ((128, 113), (1, -17, 32)),
-    )
-    for modArg, formArg in regras:
-        if mod(modArg, x):
-            return formula(formArg, x)
+def collatz_2(x: int) -> int:
+    for modulus_arguments, formula_arguments in COLLATZ_RULES:
+        if mod(modulus_arguments, x):
+            return formula(formula_arguments, x)
+    return x
 
 
-def mostra(lista):
-    plt.plot(lista)
-    plt.show()
+def plot_points(lista: list[int]) -> None:
+    plt.plot(lista, "o")  # type: ignore
+    plt.show()  # type: ignore
 
 
-def mostraP(lista):
-    plt.plot(lista, "o")
-    plt.show()
-
-
-def collPassos(x):
-    lista = []
-    while x not in lista:
-        lista.append(x)
+def count_collatz_steps(x: int) -> int:
+    collatz_sequence: list[int] = []
+    while x not in collatz_sequence:
+        collatz_sequence.append(x)
         x = collatz(x)
-    return len(lista)
+    return len(collatz_sequence)
 
 
-def wazPassos(x):
-    lista = []
-    while x not in lista:
-        lista.append(x)
-        x = wazoo(x)
-    return len(lista)
+def count_collatz_2_steps(x: int) -> int:
+    collatz_2_sequence: list[int] = []
+    while x not in collatz_2_sequence:
+        collatz_2_sequence.append(x)
+        x = collatz_2(x)
+    return len(collatz_2_sequence)
 
 
-def collatzItera(n, x):
-    for a in range(n):
+def iterate_collatz(n: int, x: int) -> int:
+    for _ in range(n):
         x = collatz(x)
     return x
 
 
-def wazooItera(n, x):
-    for a in range(n):
-        x = wazoo(x)
+def iterate_collatz_2(n: int, x: int) -> int:
+    for _ in range(n):
+        x = collatz_2(x)
     return x
-
 
 
 def main() -> None:
-    mostraP([wazPassos(a) for a in range(100)])
-    mostraP([collPassos(a) for a in range(100)])
-    # for n in range(1,100):
-    #    mostraP([wazooItera(n,a) for a in range(100)])
-    #    mostraP([collatzItera(n,a) for a in range(100)])
+    plot_points([count_collatz_2_steps(a) for a in range(100)])
+    plot_points([count_collatz_steps(a) for a in range(100)])
+    for n in range(1, 100):
+        plot_points([iterate_collatz_2(n, a) for a in range(100)])
+        plot_points([iterate_collatz(n, a) for a in range(100)])
 
 
 if __name__ == "__main__":
