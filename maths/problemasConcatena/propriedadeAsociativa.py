05af9f6a9dc5f1ec660e160abe9200a606e92d47
Camilla Alves <arcamilla42@gmail.com>
2026-02-25 16:20:34 -0300

    refactor: simplify and clarify logic in testaBase function

--- maths/problemasConcatena/propriedadeAsociativa.py
+++ maths/problemasConcatena/propriedadeAsociativa.py
@@ -1,26 +1,25 @@
-def testaBase(num, zero=False, iguais=False):
-    for a in range(1, num):
-        for b in range(num):
-            for c in range(num):
-                primeiro = (a * num + b) * c
-                segundo = a * int(b * num + c)
-                if primeiro == segundo:
-                    if zero:
-                        if primeiro == 0:
-                            continue
-                    if iguais:
-                        if (a == b) and (b == c):
-                            continue
-                    print(f"{a}|{b}|{c}")
-                    print(primeiro)
-                    print("")
-
+def testaBase(base: int, check_equal_values: bool = False) -> None:
+    for digit_1 in range(1, base):
+        for digit_2 in range(base):
+            for digit_3 in range(base):
+                primeiro = (digit_1 * base + digit_2) * digit_3
+                segundo = digit_1 * int(digit_2 * base + digit_3)
+                if primeiro != segundo:
+                    continue
+                if primeiro == 0:
+                    continue
+                if check_equal_values:
+                    if (digit_1 == digit_2) and (digit_2 == digit_3):
+                        continue
+                print(f"{digit_1}|{digit_2}|{digit_3}")
+                print(primeiro)
+                print("")
 
 
 def main() -> None:
     for base in range(2, 21):
         print(f"base {base} :\n")
-        testaBase(base, zero=True, iguais=True)
+        testaBase(base, check_equal_values=False)
 
 
 if __name__ == "__main__":
