7ce1e9ce501e08abc700081349b7ac6030a817d6
Camilla Reis <camillebay25@gmail.com>
2026-02-24 23:41:20 -0300

    refactor: rename functions and improve type hints in aproximaKinchinTrueFraction.py

--- maths/fracaoContinuas/aproximaKinchinTrueFraction.py
+++ maths/fracaoContinuas/aproximaKinchinTrueFraction.py
@@ -1,43 +1,45 @@
-def fractionToNumber(fraction):
-    quant=len(fraction)
-    a=fraction[-1]
-    print(a)
-    print()
-    for n in range(quant-1):
-        print(quant-n-2)
-        print(fraction[quant-n-2])
-        a=fraction[quant-n-2]+1/a
-        print(a)
-        print()
-    return a
+KINCHIN_CONSTANT = 2.68545200106530644530971483548179569382038
 
-def khinchinPerto(fraction,menor):
-    khinchin=2.68545200106530644530971483548179569382038
-    result=1
-    for a in fraction:
-        result*=a
-    if(menor):
-        if(result<khinchin):
+def fraction_to_number(fraction: list[int]) -> float:
+    fraction_length = len(fraction)
+    current_fraction_value = float(fraction[-1])
+    print(current_fraction_value)
+    print()
+    for depth in range(fraction_length - 1):
+        term_index = fraction_length - depth - 2
+        print(term_index)
+        print(fraction[term_index])
+        current_fraction_value = fraction[term_index] + 1 / current_fraction_value
+        print(current_fraction_value)
+        print()
+    return current_fraction_value
+
+
+def check_kinchin_proximity(fraction: list[int], is_less_than_kinchin: bool) -> bool:
+    result = 1
+    for fraction_term in fraction:
+        result *= fraction_term
+    if is_less_than_kinchin:
+        if result < KINCHIN_CONSTANT:
             return True
     else:
-        if(result>khinchin):
+        if result > KINCHIN_CONSTANT:
             return True
     return False
 
 
 def main() -> None:
-    lista=[]
-    for b in range(10):
-        a=0
+    continued_fraction: list[int] = []
+    for depth in range(10):
+        current_term = 0
         while True:
-            a+=1
-            lista.append(a)
-            if khinchinPerto(lista,b%2):
-                lista.pop()
+            current_term += 1
+            continued_fraction.append(current_term)
+            if check_kinchin_proximity(continued_fraction, depth % 2 == 0):
+                continued_fraction.pop()
                 break
-            else:
-                lista.pop()
-        print(lista)
+            continued_fraction.pop()
+        print(continued_fraction)
 
 
 if __name__ == "__main__":
