1893afb1e8ea4ca66dd1b794a3143f9f44da6285
Camilla Alves <arcamilla42@gmail.com>
2026-02-25 15:10:55 -0300

    refactor: streamline base conversion and printing logic in nToBaseSimples.py

--- maths/nToBase/nToBaseSimples.py
+++ maths/nToBase/nToBaseSimples.py
@@ -1,40 +1,35 @@
 import math
 
-
-def nToBase(n, base):
-    global LIMITE
-    sequencia = []
-    potenciaMaior = int(math.log(n, base))
-    for potencia in range(potenciaMaior, LIMITE - 1, -1):
-        termo = base**potencia
-        fator = int(n / termo)
-        n -= fator * termo
-        sequencia.append(str(fator))
-        if potencia == 0:
-            if LIMITE != 0:
-                sequencia.append(",")
-    numero = "".join(sequencia)
-    return numero
+LIMITE = -100
+DEFAULT_BASE = 11
 
 
-def impressao(n, base, tamanhoBase=0, tamanhoN=3):
-    global LIMITE
-    if tamanhoBase == 0:
-        tamanhoBase = 10 - LIMITE
-    resultado = nToBase(n, base)
-    mensagem = []
-    mensagem.append(str(base))
-    mensagem.append((f" : {n:0{tamanhoN}} : "))
-    mensagem.append((f" {:0>{tamanhoBase}}").format(resultado))
-    print("".join(mensagem))
+def convert_to_base(number: int) -> str:
+    base_digits: list[str] = []
+    highest_exponent = int(math.log(number, DEFAULT_BASE))
+    for exponent in range(highest_exponent, LIMITE - 1, -1):
+        base_term = DEFAULT_BASE**exponent
+        base_factor = number // base_term
+        number -= base_factor * base_term
+        base_digits.append(str(base_factor))
+        if exponent == 0:
+            base_digits.append(",")
+    base_number_string = "".join(base_digits)
+    return base_number_string
 
+
+def print_number_in_base(index: int, base_length: int = 0, formatted_width: int = 3):
+    if base_length == 0:
+        base_length = 10 - LIMITE
+    converted_number = convert_to_base(index)
+    print(
+        f"{DEFAULT_BASE} : {index:0{formatted_width}} :  {converted_number:0>{base_length}}"
+    )
 
 
 def main() -> None:
-    LIMITE = -100
-    base = 11
-    for n in range(1, 101):
-        impressao(n, base)
+    for index in range(1, 101):
+        print_number_in_base(index)
 
 
 if __name__ == "__main__":
