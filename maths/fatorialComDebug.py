4a3bd9d1d589a4e48d1f3f7a508eeb0a839fa135
Camilla Alves <arcamilla42@gmail.com>
2026-02-25 14:48:40 -0300

    refactor: add type hints and improve logging format in fatorialComDebug.py

--- maths/fatorialComDebug.py
+++ maths/fatorialComDebug.py
@@ -6,13 +6,13 @@ logging.basicConfig(
 logging.debug("Start of program")
 
 
-def factorial(n):
-    logging.debug("Start of factorial(%s%%)" % (n))
+def factorial(n: int) -> int:
+    logging.debug(f"Start of factorial({n})")
     total = 1
     for i in range(1, n + 1):
         total *= i
         logging.debug(f"i is {i}, total is {total}")
-    logging.debug("End of factorial(%s%%)" % (n))
+    logging.debug(f"End of factorial({n})")
     return total
 
 
