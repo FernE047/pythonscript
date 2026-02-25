78e6ec3154b66f7df0af8817b5c2c8d7a1e2cbef
Camilla Alves <arcamilla42@gmail.com>
2026-02-25 16:20:28 -0300

    refactor: improve variable naming and formatting in multplicacaoIgualConcatena.py

--- maths/problemasConcatena/multplicacaoIgualConcatena.py
+++ maths/problemasConcatena/multplicacaoIgualConcatena.py
@@ -1,15 +1,16 @@
+LIMITE = 10000
 
-    
-def main() -> None:limite = 10000
-    for a in range(1, limite + 1):
-        for b in range(a, limite + 1):
-            numMult = a * b
-            numStr = int(str(a) + str(b))
-            if numMult == numStr:
-                print(numMult)
-                print(numStr)
-                print(f"{a} | {b}")
+def main() -> None:
+    for factor_1 in range(1, LIMITE + 1):
+        for factor_2 in range(factor_1, LIMITE + 1):
+            product = factor_1 * factor_2
+            concatenated_factors = int(str(factor_1) + str(factor_2))
+            if product == concatenated_factors:
+                print(product)
+                print(concatenated_factors)
+                print(f"{factor_1} | {factor_2}")
                 print("")
+
 
 if __name__ == "__main__":
     main()
