dffbaeea7f4b6e9285ca0dc892cc5e6006b4936f
Camilla Alves <arcamilla42@gmail.com>
2026-02-28 11:32:48 -0300

    refactor: rename function and improve logic in 100DemonetizedYoutubersUntilWinnerNoProbs.py

--- probabilidades/100DemonetizedYoutubersUntilWinnerNoProbs.py
+++ probabilidades/100DemonetizedYoutubersUntilWinnerNoProbs.py
@@ -1,28 +1,32 @@
 from random import sample
 from time import time
 
-def tentativaFalha():
-    for participante in range(51):
-        if(participante not in sample(range(participante,100),50)):
-            return(True)
-    return(False)
+# Last Excecution Data :
+
+# average 100k : 0.00045391589059292357 segundos
+
+def attempt_failure() -> bool:
+    for participant_index in range(51):
+        if participant_index not in sample(range(participant_index, 100), 50):
+            return True
+    return False
 
 
 def main() -> None:
-    total=0
-    comeco=time()
+    attempt_count = 0
+    start_time = time()
     try:
-        while(tentativaFalha()):
-            total+=1
-    except:
-        fim=time()
-        duracao=fim-comeco
-        print(f"total : {total}")
-        print(f"execução total : {duracao}")
-        print(f"media total :    {duracao / total}")
-        print(f"100k :    {(duracao * 100000) / total}")
-        total=input()
-    # media 100k : 0.00045391589059292357 segundos
+        while attempt_failure():
+            attempt_count += 1
+    except KeyboardInterrupt:
+        pass
+    end_time = time()
+    execution_duration = end_time - start_time
+    print(f"total : {attempt_count}")
+    print(f"total execution : {execution_duration}")
+    print(f"total average :    {execution_duration / attempt_count}")
+    print(f"100k :    {(execution_duration * 100000) / attempt_count}")
+    input("press enter to exit")
 
 
 if __name__ == "__main__":
