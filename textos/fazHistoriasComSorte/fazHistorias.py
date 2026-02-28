77874593e57a3d989d79bc3aa5442fb8bb2aeadb
Camilla Reis <camillebay25@gmail.com>
2026-02-28 18:05:29 -0300

    refactor: rename functions for clarity and improve code structure in fazHistorias.py

--- textos/fazHistoriasComSorte/fazHistorias.py
+++ textos/fazHistoriasComSorte/fazHistorias.py
@@ -1,49 +1,57 @@
 import requests
 import bs4
-import re
 import random
 
+# google changed way too much, this script is not working anymore
 
-def conecta(site: str) -> requests.Response:
-    siteBaguncado = requests.get(site)
-    while siteBaguncado.status_code != requests.codes.ok:
-        siteBaguncado = requests.get(site)
-    return siteBaguncado
+def get_response_from_site(url: str) -> requests.Response:
+    response = requests.get(url)
+    while response.status_code != requests.codes.ok:
+        response = requests.get(url)
+    return response
 
 
-def pesquisaGoogle(search: str, adicao: str = "%20full%20lyrics") -> bs4.ResultSet[bs4.element.Tag]:
-    musicaSearch = conecta(f"https://www.google.com.br/search?q={search}{adicao}")
-    musicaSearchSoup = bs4.BeautifulSoup(musicaSearch.text, features="html.parser")
-    informacao = musicaSearchSoup.select(".r")
-    return informacao
+def search_google(query: str, adicao: str) -> bs4.ResultSet[bs4.element.Tag]:
+    google_search = get_response_from_site(
+        f"https://www.google.com.br/search?q={query}{adicao}"
+    )
+    google_search_soup = bs4.BeautifulSoup(google_search.text, features="html.parser")
+    results = google_search_soup.select(".r")
+    return results
 
-def tamanhoParaTitulo():
-    population = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17]
-    weight = [1,1,6,16,35,63,49,33,30,28,39,37,30,15,5,1]
-    return(random.choices(population,weights=weight))
 
-def procuraTextoEmLista(palavra,lista):
-    
+def tamanho_para_titulo():
+    population = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17]
+    weight = [1, 1, 6, 16, 35, 63, 49, 33, 30, 28, 39, 37, 30, 15, 5, 1]
+    return random.choices(population, weights=weight)[0]
+
+
+def procura_texto_em_lista(word: str, list_str: list[str]) -> int:
+    try:
+        return list_str.index(word)
+    except ValueError:
+        return -1
 
 
 def main() -> None:
     while True:
-        print("\n\nqual o primeiro termo?")
-        termo=input().lower()
-        tituloTamanho=tamanhoParaTitulo()
-        print(f"tamanho do titulo {tituloTamanho} palavras")
-        informacao=pesquisaGoogle(termo,adicao="+site%3A%2Ffanfiction.com.br%2F")
-        tituloOficial=[termo]
-        titulos=[]
-        while (len(tituloOficial)<=tituloTamanho):
-            for info in informacao:
-                newInfo=info.select(".LC20lb")
-                if(newInfo):
-                    texto=newInfo[0].get_text().lower()
-                    indice=procuraTextoEmLista(termo,texto.split())
-                    titulos+=[texto]
+        print("\n\nenter a term to search for fanfics in fanfiction.com.br\n")
+        term = input().lower()
+        title_size = tamanho_para_titulo()
+        print(f"title size : {title_size} words")
+        results = search_google(term, "+site%3A%2Ffanfiction.com.br%2F")
+        title: list[str] = [term]
+        titles: list[str] = []
+        while len(title) <= title_size:
+            for info in results:
+                result_title_tag = info.select(".LC20lb")
+                if result_title_tag:
+                    result_title = result_title_tag[0].get_text().lower()
+                    text_index = procura_texto_em_lista(term, result_title.split())
+                    del text_index  # only to stop lint complains
+                    titles += [result_title]
         print("\ntitulo\n")
-                
+
 
 if __name__ == "__main__":
     main()
