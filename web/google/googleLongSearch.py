8093be2289144a353d341e8a385358723396f76c
Camilla Alves <arcamilla42@gmail.com>
2026-03-02 12:01:29 -0300

    refactor: simplify functions and improve variable naming in googleLongSearch.py

--- web/google/googleLongSearch.py
+++ web/google/googleLongSearch.py
@@ -3,126 +3,73 @@ import bs4
 import re
 import time
 
+# this script is based on a game I used to play. The idea is to find the longest search term that has more results than the previous one. For example, if you start with "a", it will check "a ", "aA", "aB", ..., "aZ", "aa", "ab", ..., and so on, until it finds a term that has fewer results than the previous one. Then it will return the longest term found.
 
-def resultadosQuantia(termo: str) -> int:
-    informacao = siteProcura(
-        f"https://www.google.com.br/search?q={termo}", "#resultStats"
+
+def connect(url: str) -> requests.Response:
+    response = requests.get(url)
+    while response.status_code != requests.codes.ok:
+        response = requests.get(url)
+    return response
+
+
+def fetch_site_data(url: str, css_selector: str) -> bs4.ResultSet[bs4.element.Tag]:
+    response = connect(url)
+    site_soup = bs4.BeautifulSoup(response.text, features="html.parser")
+    tags = site_soup.select(css_selector)
+    return tags
+
+
+def get_search_results_count(search_query: str) -> int:
+    fetched_data = fetch_site_data(
+        f"https://www.google.com.br/search?q={search_query}", "#resultStats"
     )
-    pegaNumero = re.compile(r"\d{1,3}")
-    textoMisturado = informacao[0].getText()
-    if textoMisturado:
-        numeroTexto = pegaNumero.findall(textoMisturado)
-        numero = int("".join(numeroTexto))
-    else:
-        numero = 0
-    print(numero)
-    return numero
+    result_count_regex = re.compile(r"\d{1,3}")
+    raw_result_text = fetched_data[0].getText()
+    if raw_result_text:
+        result_count_matches = result_count_regex.findall(raw_result_text)
+        return int("".join(result_count_matches))
+    return 0
 
 
-def encontrarLong(termo, longest=0):
-    proxima = [
-        " ",
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
-        "M",
-        "N",
-        "O",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-    ]
-    contagem = []
-    maior = 0
-    for letra in proxima:
-        contagem.append(0)
-    for letra in range(len(proxima)):
-        termo = list(termo)
-        termo.append(proxima[letra])
-        termo = str("".join(termo))
-        print(termo)
-        quantidade = resultadosQuantia(termo)
-        contagem[letra] = quantidade
-        if quantidade >= contagem[maior]:
-            maior = letra
-        termo = list(termo)
-        del termo[(len(termo) - 1)]
-        termo = "".join(termo)
-    if contagem[maior] <= longest:
-        print(f"\n\n O Termo {termo} tem {longest} resultados")
-        return 0
-    else:
-        termo = list(termo)
-        termo.append(proxima[maior])
-        termo = str("".join(termo))
-    return encontrarLong(termo, contagem[maior])
-
-
-def conecta(site: str) -> requests.Response:
-    siteBaguncado = requests.get(site)
-    while siteBaguncado.status_code != requests.codes.ok:
-        siteBaguncado = requests.get(site)
-    return siteBaguncado
-
-
-def siteProcura(site: str, html: str) -> bs4.ResultSet[bs4.element.Tag]:
-    siteBaguncado = conecta(site)
-    siteSoup = bs4.BeautifulSoup(siteBaguncado.text, features="html.parser")
-    informacao = siteSoup.select(html)
-    return informacao
+def find_longest_term(search_term: str, current_longest_count: int = 0) -> None:
+    possible_search_chars = list(
+        " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    )
+    search_results_count: list[int] = []
+    max_results_index = 0
+    for char in possible_search_chars:
+        search_results_count.append(0)
+    for char_index, char in enumerate(possible_search_chars):
+        search_chars = list(search_term)
+        search_chars.append(char)
+        search_term = str("".join(search_chars))
+        print(f"Searching for '{search_term}'...")
+        result_count = get_search_results_count(search_term)
+        print(f"{result_count} results")
+        search_results_count[char_index] = result_count
+        if result_count >= search_results_count[max_results_index]:
+            max_results_index = char_index
+        search_chars = list(search_term)
+        del search_chars[(len(search_chars) - 1)]
+        search_term = "".join(search_chars)
+    if search_results_count[max_results_index] <= current_longest_count:
+        print(f"\n\n The Term {search_term} has {current_longest_count} results")
+        return
+    search_chars = list(search_term)
+    search_chars.append(possible_search_chars[max_results_index])
+    search_term = str("".join(search_chars))
+    find_longest_term(search_term, search_results_count[max_results_index])
 
 
 def main() -> None:
-    print("digite o termo de pesquisa")
-    termoInicial = input()
-    startTime = time.time()
-    encontrarLong(termoInicial)
-    endTime = time.time()
-    realTime = endTime - startTime
-    print(f"levou {realTime} segundos")
+    print("enter the initial term:")
+    initial_term = input()
+    start_time = time.time()
+    find_longest_term(initial_term)
+    end_time = time.time()
+    real_time = end_time - start_time
+    print(f"it took {real_time} seconds")
 
 
 if __name__ == "__main__":
