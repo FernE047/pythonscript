36e0418cb9c675e1242e68bf4f23e5909577b2ba
Camilla Alves <arcamilla42@gmail.com>
2026-03-02 10:55:08 -0300

    refactor: streamline image processing in melhorar imagem.py for improved readability and maintainability

--- textos/ocrTeste/melhorando imagem.py
+++ textos/ocrTeste/melhorando imagem.py
@@ -1,8 +1,10 @@
-import pytesseract as ocr
 import numpy as np
 import cv2
 
 from PIL import Image
+
+INPUT_IMAGE_PATH = "input.jpg"
+OUTPUT_IMAGE_PATH = "output.jpg"
 
 
 def open_image_as_rgba(image_path: str) -> Image.Image:
@@ -14,29 +16,14 @@ def open_image_as_rgba(image_path: str) -> Image.Image:
 
 
 def main() -> None:
-    # tipando a leitura para os canais de ordem RGB
-    imagem = open_image_as_rgba("pictureTolerancia.jpg")
-
-    # convertendo em um array editável de numpy[x, y, CANALS]
-    npimagem = np.asarray(imagem).astype(np.uint8)
-
-    # diminuição dos ruidos antes da binarização
-    npimagem[:, :, 0] = 0  # zerando o canal R (RED)
-    npimagem[:, :, 2] = 0  # zerando o canal B (BLUE)
-
-    # atribuição em escala de cinza
-    im = cv2.cvtColor(npimagem, cv2.COLOR_RGB2GRAY)
-
-    # aplicação da truncagem binária para a intensidade
-    # pixels de intensidade de cor abaixo de 127 serão convertidos para 0 (PRETO)
-    # pixels de intensidade de cor acima de 127 serão convertidos para 255 (BRANCO)
-    # A atrubição do THRESH_OTSU incrementa uma análise inteligente dos nivels de truncagem
-    ret, thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    # reconvertendo o retorno do threshold em um objeto do tipo PIL.Image
-    binimagem = Image.fromarray(thresh)
-
-    binimagem.save("pictureBetter1.jpg")
+    image = open_image_as_rgba(INPUT_IMAGE_PATH)
+    np_image = np.asarray(image).astype(np.uint8)
+    np_image[:, :, 0] = 0
+    np_image[:, :, 2] = 0
+    im = cv2.cvtColor(np_image, cv2.COLOR_RGB2GRAY)
+    _, thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    bin_image = Image.fromarray(thresh)
+    bin_image.save(OUTPUT_IMAGE_PATH)
 
 
 if __name__ == "__main__":
