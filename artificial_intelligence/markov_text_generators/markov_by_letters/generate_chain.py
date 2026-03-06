af6f3c79c9c19a086343a0f11e24306b57c1ac35
Camilla Alves <arcamilla42@gmail.com>
2026-03-06 15:05:45 -0300

    perf: the chain is only write in the file once

--- artificial_intelligence/markov_text_generators/markov_by_letters/generate_chain.py
+++ artificial_intelligence/markov_text_generators/markov_by_letters/generate_chain.py
@@ -5,54 +5,32 @@ EMPTY_CHAR = "¨"
 INPUT_FILE = CLEAN_INPUT_FILE
 CHAIN_FOLDER = Path("chain")
 
-
-def is_chain_file_empty(index: int) -> bool:
-    for file in CHAIN_FOLDER.iterdir():
-        if f"{index:03d}.txt" == file.name:
-            return False
-    return True
+MarkovChain = dict[str, int]
+MarkovCollection = dict[int, MarkovChain]
 
 
-def update_chain_file(index: int, keyword: str) -> None:
-    chain_path = CHAIN_FOLDER / f"{index:03d}.txt"
-    if is_chain_file_empty(index):
-        with open(chain_path, "w", encoding="utf-8") as file_write:
-            file_write.write(f"{keyword} 1\n")
-            return
-    with open(chain_path, "r", encoding="utf-8") as file_read:
-        lines = file_read.readlines()
-    new_lines = update_keyword_count(keyword, lines)
-    with open(chain_path, "w", encoding="utf-8") as file_write:
-        file_write.writelines(new_lines)
+def update_chain_file(index: int, markov_chain: MarkovChain) -> None:
+    with open(CHAIN_FOLDER / f"{index:03d}.txt", "w", encoding="utf-8") as chain_file:
+        for char, count in markov_chain.items():
+            chain_file.write(f"{char} {count}\n")
 
 
-def update_keyword_count(keyword: str, lines: list[str]) -> list[str]:
-    keyword_found = False
-    index = len(keyword)
-    new_lines: list[str] = []
-    for line in lines:
-        if not line:
-            continue
-        if line[:index] != keyword:
-            new_lines.append(line)
-            continue
-        count = int(line[index + 1 :]) + 1
-        new_lines.append(f"{line[: index + 1]}{count}\n")
-        keyword_found = True
-    if not keyword_found:
-        new_lines.append(f"{keyword} 1\n")
-    return new_lines
-
-
-def process_message(message: str, index: int) -> None:
+def process_message(
+    markov_collection: MarkovCollection, message: str, index: int
+) -> None:
     message_length = len(message)
     if index >= message_length:
         return
     character = message[index]
     if character == "\n":
         return
+    if index not in markov_collection:
+        markov_collection[index] = {}
+    if index + 1 not in markov_collection:
+        markov_collection[index + 1] = {}
+    markov_chain = markov_collection[index]
     if index == 0:
-        update_chain_file(index, character)
+        markov_collection[index][character] = markov_chain.get(character, 0) + 1
     if message_length >= 1:
         try:
             next_character = message[index + 1]
@@ -60,13 +38,18 @@ def process_message(message: str, index: int) -> None:
             next_character = EMPTY_CHAR
         if next_character == "\n":
             next_character = EMPTY_CHAR
-        update_chain_file(index + 1, f"{character} {next_character}")
+        markov_collection[index + 1][f"{character} {next_character}"] = (
+            markov_chain.get(f"{character} {next_character}", 0) + 1
+        )
 
 
 def generate_character_chain() -> None:
     with open(INPUT_FILE, "r", encoding="utf-8") as file:
         lines = [line.strip() for line in file.readlines()]
+    markov_collection: MarkovCollection = {}
     for message in lines:
         message_length = len(message)
         for index in range(message_length):
-            process_message(message, index)
+            process_message(markov_collection, message, index)
+    for index, markov_chain in markov_collection.items():
+        update_chain_file(index, markov_chain)
